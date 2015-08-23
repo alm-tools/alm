deae046a109d614321aefdee68a1635f9da59068
basarat <basaratali@gmail.com>
2015-08-23 12:01:53 +1000

    a better dark theme

--- src/app/styles/theme.tsx
+++ src/app/styles/theme.tsx
@@ -222,64 +222,83 @@ export let LightTheme = {
     },
 };
 
+function getPalette() {
+    return {
+        primary1Color: Colors.cyan500,
+        primary2Color: Colors.cyan700,
+        primary3Color: Colors.cyan100,
+        accent1Color: Colors.pinkA200,
+        accent2Color: Colors.pinkA400,
+        accent3Color: Colors.pinkA100,
+        textColor: Colors.darkBlack,
+        canvasColor: Colors.white,
+        borderColor: Colors.grey300,
+        disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
+    };
+};
 
 export let DarkTheme = {
-  getPalette() {
-    return {
-      textColor: Colors.fullWhite,
-      canvasColor: '#303030',
-      borderColor:  ColorManipulator.fade(Colors.fullWhite, 0.3), //Colors.grey300
-      disabledColor: ColorManipulator.fade(Colors.fullWhite, 0.3),
-      primary1Color: Colors.teal200,
-    };
-  },
-  getComponentThemes(palette) {
-    let cardColor = Colors.grey800;
-    return {
-      avatar: {
-        borderColor: 'rgba(0, 0, 0, 0.5)',
-      },
-      floatingActionButton: {
-        disabledColor: ColorManipulator.fade(palette.textColor, 0.12),
-      },
-      leftNav: {
-        color: cardColor,
-      },
-      menu: {
-        backgroundColor: cardColor,
-        containerBackgroundColor: cardColor,
-      },
-      menuItem: {
-        hoverColor: 'rgba(255, 255, 255, .03)',
-      },
-      menuSubheader: {
-        borderColor: 'rgba(255, 255, 255, 0.3)',
-      },
-      paper: {
-        backgroundColor: cardColor,
-      },
-      raisedButton: {
-        color: Colors.grey500,
-      },
-      toggle: {
-        thumbOnColor: Colors.cyan200,
-        thumbOffColor: Colors.grey400,
-        thumbDisabledColor: Colors.grey800,
-        thumbRequiredColor: Colors.cyan200,
-        trackOnColor: ColorManipulator.fade(Colors.cyan200, 0.5),
-        trackOffColor: 'rgba(255, 255, 255, 0.3)',
-        trackDisabledColor: 'rgba(255, 255, 255, 0.1)',
-      },
-      refreshIndicator: {
-        strokeColor: Colors.grey700,
-        loadingStrokeColor: Colors.teal300,
-      },
-      slider: {
-        trackColor: Colors.minBlack,
-        handleColorZero: cardColor,
-        handleFillColor: cardColor,
-        selectionColor: Colors.cyan200,
-      },
-    };
-  },
+    getPalette() {
+        return {
+            primary1Color: Colors.grey600,
+            primary2Color: Colors.grey800,
+            primary3Color: Colors.grey100,
+            accent1Color: Colors.pinkA200,
+            accent2Color: Colors.pinkA400,
+            accent3Color: Colors.pinkA100,
+            textColor: Colors.fullWhite,
+            canvasColor: '#303030',
+            borderColor: ColorManipulator.fade(Colors.fullWhite, 0.3), //Colors.grey300
+            disabledColor: ColorManipulator.fade(Colors.fullWhite, 0.3),
+        };
+    },
+    getComponentThemes(palette) {
+        let cardColor = Colors.grey800;
+        return {
+            avatar: {
+                borderColor: 'rgba(0, 0, 0, 0.5)',
+            },
+            floatingActionButton: {
+                disabledColor: ColorManipulator.fade(palette.textColor, 0.12),
+            },
+            leftNav: {
+                color: cardColor,
+            },
+            menu: {
+                backgroundColor: cardColor,
+                containerBackgroundColor: cardColor,
+            },
+            menuItem: {
+                hoverColor: 'rgba(255, 255, 255, .03)',
+            },
+            menuSubheader: {
+                borderColor: 'rgba(255, 255, 255, 0.3)',
+            },
+            paper: {
+                backgroundColor: cardColor,
+            },
+            raisedButton: {
+                color: Colors.grey500,
+            },
+            toggle: {
+                thumbOnColor: Colors.cyan200,
+                thumbOffColor: Colors.grey400,
+                thumbDisabledColor: Colors.grey800,
+                thumbRequiredColor: Colors.cyan200,
+                trackOnColor: ColorManipulator.fade(Colors.cyan200, 0.5),
+                trackOffColor: 'rgba(255, 255, 255, 0.3)',
+                trackDisabledColor: 'rgba(255, 255, 255, 0.1)',
+            },
+            refreshIndicator: {
+                strokeColor: Colors.grey700,
+                loadingStrokeColor: Colors.teal300,
+            },
+            slider: {
+                trackColor: Colors.minBlack,
+                handleColorZero: cardColor,
+                handleFillColor: cardColor,
+                selectionColor: Colors.cyan200,
+            },
+        };
+    },
 };
