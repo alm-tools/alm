69a4e4318e5fe63d5ed1cc1d82587df130810a94
Basarat Syed <basaratali@gmail.com>
2015-08-25 17:13:18 +1000

    probably glob it will be

--- src/server/cache/fileListingWorker.ts
+++ src/server/cache/fileListingWorker.ts
@@ -1,10 +1,18 @@
 /**
  * This file will start working as soon as you import it
  * 
- * It will list out all the files (fi) / (fo) folders into a file listings.txt
+ * It will list out all the files into listings.txt
  * 
  * It will watch the file system for changes, and if there are any changes, 
  * it will list the files again, writing only at the end...and only if something changed
  */
 
 import chokidar = require('chokidar');
+
+function processAllFiles(){
+    
+}
+
+// other research 
+// https://github.com/coolaj86/node-walk
+// https://github.com/isaacs/node-glob
