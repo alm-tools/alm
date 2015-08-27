6a9cf709edb5eef444e37e6e8d22bc6cc534075a
Basarat Syed <basaratali@gmail.com>
2015-08-27 12:49:43 +1000

    some omnisearch ui work

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -3,6 +3,7 @@ import {BaseComponent, RaisedButton, AppBar, MenuItem, LeftNav} from "./ui";
 import * as csx from "csx";
 import {TabsContainer} from "./tabs/tabsContainer";
 import * as commands from "./commands/commands";
+var Modal = require('react-modal');
 
 let menuItems = [
     { route: 'get-started', text: 'Get Started' },
@@ -16,10 +17,16 @@ let menuItems = [
     },
 ];
 
-export class Root extends BaseComponent<{}, {}>{
+export interface State {
+    isOmniSearchOpen?: boolean
+}
+
+export class Root extends BaseComponent<{}, State>{
 
     constructor(props: {}) {
         super(props);
+        
+        this.state = {};
     }
 
     refs: { [string: string]: any; leftNav?: any; }
@@ -31,9 +38,11 @@ export class Root extends BaseComponent<{}, {}>{
     componentDidMount(){
         commands.findFile.on(()=>{
             console.log('find file');
+            this.openOmniSearch();
         });
         commands.findCommand.on(()=>{
             console.log('find command');
+            this.openOmniSearch();
         });
     }
 
@@ -47,9 +56,26 @@ export class Root extends BaseComponent<{}, {}>{
                 // />
                 }
                 <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
+                <Modal
+                      isOpen={this.state.isOmniSearchOpen}
+                      onRequestClose={this.closeOmniSearch}
+                    >
+                      <h2>Hello</h2>
+                      <button onClick={this.closeOmniSearch}>close</button>
+                      <div>I am a modal</div>
+                      <form>
+                        <input />
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                      </form>
+                    </Modal>
 
                 <TabsContainer/>
             </div>;
     }
-
+    
+    closeOmniSearch = () => this.setState({ isOmniSearchOpen: false });
+    openOmniSearch = () => this.setState({isOmniSearchOpen: true});
 }
