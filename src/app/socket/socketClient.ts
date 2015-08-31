87df487eea8bb9ca90a426bdd0bd1184f8629f18
Basarat Syed <basaratali@gmail.com>
2015-08-31 16:05:23 +1000

    better

--- src/app/socket/socketClient.ts
+++ src/app/socket/socketClient.ts
@@ -1,9 +1,9 @@
+// Provide the following service to the server
 import {Client} from "../../socketLib/socketLibClient";
-import {service} from "../../server/socket/serviceServerContract";
 import * as serviceClient from "./serviceClient";
-
 let client = new Client(serviceClient);
 
 // Consume the following services from the server
+import {service} from "../../server/socket/serviceServerContract";
 export let echo = client.sendToSocket(service.echo);
 export let getAllFiles = client.sendToSocket(service.getAllFiles);
