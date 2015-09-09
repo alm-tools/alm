4d7c92abc0f05d99f581fa2887d1f6c0a725ac94
Basarat Syed <basaratali@gmail.com>
2015-09-09 12:20:51 +1000

    seperate out the hint function

--- src/app/codemirror/codeEditor.tsx
+++ src/app/codemirror/codeEditor.tsx
@@ -65,6 +65,60 @@ export class CodeEditor extends React.Component<Props,any>{
 	
 	resizehandler: {dispose:()=>any};
 	componentDidMount () {
+        
+        function hint(ed,options ){
+            
+            // options is just a copy of the `hintOptions` with defaults added
+            // So do something fancy with the Editor 
+            // console.log(ed,options);
+            
+            // Delegate to the auto version for now 
+            let original: {
+                // the list of *completions*. Each completion is rendered using the Widget class in showHint
+                // The complex interface is based on reading the code of the Widget constructor
+                // Also see docs https://codemirror.net/doc/manual.html#addon_show-hint
+                list: (string | { 
+                    // Used as the completion text if hint is not provided
+                    text?: string;
+                    
+                    // Also used if render isn't provided && `displayText` isn't provided
+                    displayText?: string; 
+                    className?: string;
+                    // if a render function is provided ... it is responsible for adding the needed text to `elt`
+                    // elt is the element `li` added for this completion
+                    // data is meh .. more review needed
+                    // cur is the current completion ... i.e. this item in the list
+                    render?: (elt: HTMLLIElement, data:any, cur: any)=>void;
+                    
+                    // Called if completion is picked
+                    hint?: Function; 
+                    
+                    // More specific completion
+                    from?: CodeMirror.Position, // start of token that is being completed
+                    to?: CodeMirror.Position, // end of token that is being completed
+                })[],
+                from: CodeMirror.Position, // start of token that is being completed
+                to: CodeMirror.Position, // end of token that is being completed
+            } = (CodeMirror as any).hint.auto(ed,options);
+            
+            function render(elt: HTMLLIElement, data: any, cur: any) {
+                elt.appendChild(document.createTextNode(cur.text));
+            }
+            
+            console.log(original);
+            original.list = original.list.map(o=>{
+                let str: string = o as string;
+                return {
+                    render: render,
+                    text: str,
+                };        
+            });
+            
+            return original;
+        };
+    
+        
+        
         var options: CodeMirror.EditorConfiguration = {
             lineNumbers: true,
             mode: 'javascript',
@@ -94,56 +148,8 @@ export class CodeEditor extends React.Component<Props,any>{
             showHint: true,
             hintOptions: {
                 completeOnSingleClick: true,
-                hint: function(ed,options ){
-                    
-                    // options is just a copy of the `hintOptions` with defaults added
-                    // So do something fancy with the Editor 
-                    // console.log(ed,options);
-                    
-                    // Delegate to the auto version for now 
-                    let original: {
-                        // the list of *completions*. Each completion is rendered using the Widget class in showHint
-                        // The complex interface is based on reading the code of the Widget constructor
-                        // Also see docs https://codemirror.net/doc/manual.html#addon_show-hint
-                        list: (string | { 
-                            // Used as the completion text if hint is not provided
-                            text?: string;
-                            
-                            // Also used if render isn't provided && `displayText` isn't provided
-                            displayText?: string; 
-                            className?: string;
-                            // if a render function is provided ... it is responsible for adding the needed text to `elt`
-                            // elt is the element `li` added for this completion
-                            // data is meh .. more review needed
-                            // cur is the current completion ... i.e. this item in the list
-                            render?: (elt: HTMLLIElement, data:any, cur: any)=>void;
-                            
-                            // Called if completion is picked
-                            hint?: Function; 
-                            
-                        })[],
-                        from: CodeMirror.Position, // start of token that is being completed
-                        to: CodeMirror.Position, // end of token that is being completed
-                    } = (CodeMirror as any).hint.auto(ed,options);
-                    
-                    function render(elt: HTMLLIElement, data: any, cur: any) {
-                        elt.appendChild(document.createTextNode(cur.displayText));
-                    }
-                    
-                    console.log(original);
-                    original.list = original.list.map(o=>{
-                        let str: string = o as string;
-                        return {
-                            displayText: str,
-                            render: render,
-                            text: str,
-                        };        
-                    });
-                    
-                    return original;
-                },    
+                hint: hint,
             },
-            
             
             /** Overcomes horizontal scrolling for now */
             lineWrapping: true,
