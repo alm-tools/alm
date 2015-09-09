b88a1a8c21a09ba0c947c76e8d1627b5dcd987e9
basarat <basaratali@gmail.com>
2015-09-09 20:55:02 +1000

    :test:

---
+++ src/bas.ts
@@ -0,0 +1,6 @@
+/**
+ * This file exists for initial testing
+ * Basically I edit this file to see if it works 🌹
+ */
+// Making sure I don't *inadvertently* pollute the global namespace
+export var foo = 123;
