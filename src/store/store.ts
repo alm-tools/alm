b58e4977d90f88eed604cc8aef595e2ad75b97af
Basarat Syed <basaratali@gmail.com>
2015-09-21 14:19:34 +1000

    wip

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,2 +1,18 @@
 import redux = require('redux');
 
+interface State {
+	activeProject: string;
+}
+let initialState = { activeProject: '' };
+
+interface SetActiveProjectAction {
+	activeProject: string;
+}
+
+type Action = SetActiveProjectAction;
+
+function statusBar(state: State = initialState, action: Action): State {
+	return {
+		activeProject: action.activeProject
+	};
+}
