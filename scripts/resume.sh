e36f4dfd1201a9a18b88e04ccc246da8adcd8613
basarat <basaratali@gmail.com>
2015-10-11 11:50:41 +1100

    files again :(

---
+++ scripts/resume.sh
@@ -0,0 +1,6 @@
+#Resume work on a new machine
+
+git -c diff.mnemonicprefix=false -c core.quotepath=false fetch origin
+git -c diff.mnemonicprefix=false -c core.quotepath=false pull --rebase origin master
+
+npm install 
