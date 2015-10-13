86e25d7a5a8ee78cf252d7b1299ab0ee36af1953
Basarat <basaratali@gmail.com>
2015-10-13 17:48:51 +1100

    more thought into the select list view

--- src/app/selectListView.tsx
+++ src/app/selectListView.tsx
@@ -15,6 +15,7 @@ import * as commands from "./commands/commands";
 import {match, filter as fuzzyFilter} from "fuzzaldrin";
 
 
+type DataItem = any;
 
 export interface Props {
 }
@@ -22,24 +23,32 @@ export interface State {
     isOpen?: boolean;
     filterValue?: string;
     selectedIndex?: number;
+
+    data?: DataItem[];
+    render?: (t:DataItem) => any;
 }
 
 @ui.Radium
 export class SelectListView extends BaseComponent<Props, State>{
+
+    /** The main interaction API */
+    show(args: {
+        data: DataItem[];
+        render: (t: DataItem) => any;
+    }) {
+        this.setState({
+            isOpen: true,
+            data: args.data,
+            render: args.render
+        });
+    }
+
     maxShowCount = 15;
 
     constructor(props: Props) {
         super(props);
 
-        this.state = this.propsToState(props);
-    }
-
-    propsToState(props: Props): State {
-        return {
-        };
-    }
-    componentWillReceiveProps(props: Props) {
-        this.setState(this.propsToState(props));
+        this.state = {};
     }
 
     refs: {
@@ -49,25 +58,6 @@ export class SelectListView extends BaseComponent<Props, State>{
     }
 
     componentDidMount() {
-        server.fileList({}).then((res) => {
-            // this.relativeFilePaths = res.relativeFilePaths;
-            this.forceUpdate();
-        });
-
-        cast.fileListUpdated.on((update) => {
-            console.log(update);
-            // this.relativeFilePaths = update.relativeFilePaths;
-            this.forceUpdate();
-        });
-
-        commands.findFile.on(() => {
-            console.log('find file');
-            this.openOmniSearch();
-        });
-        commands.findCommand.on(() => {
-            console.log('find command');
-            this.openOmniSearch();
-        });
     }
 
     render() {
@@ -76,55 +66,55 @@ export class SelectListView extends BaseComponent<Props, State>{
         // let fileListRendered = fileList.map((result,i) => highlightMatch(result, this.state.filterValue, selectedIndex === i));
 
         return <Modal
-              isOpen={this.state.isOpen}
-              onRequestClose={this.closeOmniSearch}>
+            isOpen={this.state.isOpen}
+            onRequestClose={this.closeOmniSearch}>
                 <div style={[csx.vertical]}>
                     <div style={[csx.horizontal]}>
                         <h4>Omni Search</h4>
                         <div style={[csx.flex]}></div>
                         <div style={[styles.userTip]}>Press <code style={styles.keyStroke}>esc</code> to close</div>
-                    </div>
+                        </div>
 
-                    <div style={[styles.paddedTopBottom1,csx.vertical]}>
+                    <div style={[styles.paddedTopBottom1, csx.vertical]}>
                         <input
                             type="text"
                             ref="omniSearchInput"
                             placeholder="Filter"
                             onChange={this.onChangeFilter}
                             onKeyDown={this.onChangeSelected}
-                        />
-                    </div>
+                            />
+                        </div>
 
-                    <div style={[csx.vertical,csx.flex,{overflow:'auto'}]}>
+                    <div style={[csx.vertical, csx.flex, { overflow: 'auto' }]}>
                         <div style={[csx.vertical]}>
                             {/*fileListRendered*/}
-                         </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-        </Modal>
+            </Modal>
     }
 
     openOmniSearch = () => {
         this.setState({ isOpen: true });
         this.refs.omniSearchInput.getDOMNode().focus();
     };
-    closeOmniSearch = ()=>{
+    closeOmniSearch = () => {
         this.setState({ isOpen: false, filterValue: '' });
     };
-    onChangeFilter = debounce((e)=>{
+    onChangeFilter = debounce((e) => {
         let filterValue = this.refs.omniSearchInput.getDOMNode().value;
         // this.filteredResults = fuzzyFilter(this.relativeFilePaths, filterValue);
         // this.filteredResults = this.filteredResults.slice(0,this.maxShowCount);
-        this.setState({ filterValue, selectedIndex:0 });
-    },50);
+        this.setState({ filterValue, selectedIndex: 0 });
+    }, 50);
     incrementSelected = debounce(() => {
         // this.setState({ selectedIndex: rangeLimited({ num: ++this.state.selectedIndex, min: 0, max: Math.min(this.maxShowCount - 1, this.filteredResults.length - 1), loopAround: true }) });
     }, 0, true);
     decrementSelected = debounce(() => {
         // this.setState({ selectedIndex: rangeLimited({ num: --this.state.selectedIndex, min: 0, max: Math.min(this.maxShowCount - 1, this.filteredResults.length - 1), loopAround: true }) });
-    },0,true);
-    onChangeSelected = (e)=>{
-        if (e.key == 'ArrowUp'){
+    }, 0, true);
+    onChangeSelected = (e) => {
+        if (e.key == 'ArrowUp') {
             e.preventDefault();
             this.decrementSelected();
         }
@@ -132,7 +122,7 @@ export class SelectListView extends BaseComponent<Props, State>{
             e.preventDefault();
             this.incrementSelected();
         }
-        if (e.key == 'Enter'){
+        if (e.key == 'Enter') {
             e.preventDefault();
             // let relativeFilePath = this.filteredResults[this.state.selectedIndex];
             // if (relativeFilePath) {
