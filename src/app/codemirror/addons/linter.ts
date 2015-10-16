aa9258ed71a93a2aa9afc9832126a52626f3949a
Basarat <basaratali@gmail.com>
2015-10-16 18:16:59 +1100

    code error to lint error

--- src/app/codemirror/addons/linter.ts
+++ src/app/codemirror/addons/linter.ts
@@ -19,11 +19,20 @@ interface LintError {
     to: CodeMirror.Position
 }
 
+function codeErrorToLintError(codeError: CodeError): LintError {
+    return {
+        message: codeError.message,
+        severity: 'error',
+        from: codeError.from,
+        to: codeError.to
+    };
+}
+
 function lint(doc: string, cb: (cm: CodeMirror.EditorFromTextArea, errorsNotSorted: LintError[]) => void, options: any, cm: CodeMirror.EditorFromTextArea) {
     let errors: LintError[] = [];
     errors.push({
         message: 'sample',
-        severity: 'warning',
+        severity: 'error',
         from: CodeMirror.Pos(0, 0),
         to: CodeMirror.Pos(0, 10),
     });
