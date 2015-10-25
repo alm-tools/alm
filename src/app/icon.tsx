e23f5b6dcfbe21a53050ef089f10e63501666377
basarat <basaratali@gmail.com>
2015-10-25 19:50:55 +1100

    feat(font-awesome) wrap up in a component to make it easier to upgrade font-awesome in future :rose:

---
+++ src/app/icon.tsx
@@ -0,0 +1,65 @@
+import React = require("react");
+import Radium = require('radium');
+import csx = require('csx');
+import {BaseComponent} from "./ui";
+import * as ui from "./ui";
+
+export interface Props extends React.Props<any> {
+    name: string;
+    size?: string; // 1g,2x,3x,4x,5x
+    rotate?: string; // '45', '90', '135', '180', '225', '270', '315'
+    flip?: string; // vertical,horizontal
+    fixedWidth?: boolean;
+    spin?: boolean;
+    pulse?: boolean;
+    stack?: string; // 1x, 2x
+    inverse?: boolean;
+
+    className?: string; // anything else you want
+}
+export interface State {
+
+}
+
+@ui.Radium
+export class Icon extends BaseComponent<Props, State>{
+    render() {
+        let {
+            name, size, rotate, flip, spin, fixedWidth, stack, inverse,
+            pulse, className
+        } = this.props;
+        let classNames = `fa fa-${name}`;
+        if (size) {
+            classNames = `${classNames} fa-${size}`;
+        }
+        if (rotate) {
+            classNames = `${classNames} fa-rotate-${rotate}`;
+        }
+        if (flip) {
+            classNames = `${classNames} fa-flip-${flip}`;
+        }
+        if (fixedWidth) {
+            classNames = `${classNames} fa-fw`;
+        }
+        if (spin) {
+            classNames = `${classNames} fa-spin`;
+        }
+        if (pulse) {
+            classNames = `${classNames} fa-pulse`;
+        }
+
+        if (stack) {
+            classNames = `${classNames} fa-stack-${stack}`;
+        }
+        if (inverse) {
+            classNames = `${classNames} fa-inverse`;
+        }
+        if (className) {
+            classNames = `${classNames} ${className}`;
+        }
+
+        return (
+            <i className={classNames}></i>
+        );
+    }
+}
