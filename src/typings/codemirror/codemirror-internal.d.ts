299c3c051320225b2750fbe25c9f080930c4ac0f
Basarat Syed <basaratali@gmail.com>
2015-10-27 17:50:11 +1100

    codemirror internal

---
+++ src/typings/codemirror/codemirror-internal.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for CodeMirror
+// Project: https://github.com/marijnh/CodeMirror
+// Definitions by: basarat <https://github.com/basarat>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module CodeMirror {
+    /** @internal Utility function inside code mirror to stop events */
+    function e_stop(event:any);
+}
