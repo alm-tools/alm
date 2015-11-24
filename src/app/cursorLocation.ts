4642024ebae5ce1a719df6f1ab000b5f439230eb
Basarat <basaratali@gmail.com>
2015-11-25 09:23:14 +1100

    WIP cursor history

--- src/app/cursorLocation.ts
+++ src/app/cursorLocation.ts
@@ -6,6 +6,8 @@ import * as utils from "../common/utils";
 
 /** The current cursor location */
 let currentIndex = 0;
+let history: CursorHistoryEntry[] = [];
+const insignificantLines = 3;
 
 /** Subscribe to user requests to move around */
 commands.previousCursorLocation.on(()=>{
@@ -13,7 +15,14 @@ commands.previousCursorLocation.on(()=>{
 });
 commands.nextCursorLocation.on(()=>{
     next();
-})
+});
+
+interface CursorHistoryEntry {
+    tabId: string;
+    tabUrl: string;
+    firstContact: EditorPosition; // We move this around for the same cursor hitory entry
+    finalContact: EditorPosition; // This is the point that is finally saved for this entry
+}
 
 export function previous() {
     console.log('goto previous');
