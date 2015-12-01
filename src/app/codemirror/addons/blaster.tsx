bb1923edbad9bdddfe4b7dbf6747ef694be61bbf
Basarat <basaratali@gmail.com>
2015-12-02 08:49:57 +1100

    WIP new style blaster

--- src/app/codemirror/addons/blaster.tsx
+++ src/app/codemirror/addons/blaster.tsx
@@ -19,17 +19,15 @@ export class Blaster extends ui.BaseComponent<Props,any>{
 
     canvas = ():HTMLCanvasElement => this.refs['canvas'] as any;
     render(){
-        if (!this.props.cm) {
-            return <div style={{display:'none'} as any} />;
-        }
-        else {
-            return <canvas ref="canvas" />
-        }
+        let style = this.props.cm || {display:'none'};
+
+        return <canvas style={style} ref="canvas" />
     }
 
     componentWillReceiveProps(props:Props,oldProps:Props){
         if (!oldProps.cm && props.cm){
             props.cm.on('change', this.handleChange);
+            this.initCanvas();
             this.loop();
         }
     }
@@ -39,30 +37,199 @@ export class Blaster extends ui.BaseComponent<Props,any>{
         this.props.cm.off('change', this.handleChange);
     }
 
+    ctx:CanvasRenderingContext2D;
+    initCanvas = () => {
+        let canvas = this.canvas();
+ 		this.ctx = canvas.getContext('2d'),
+
+ 		canvas.style.position = 'absolute';
+ 		canvas.style.top = '0';
+ 		canvas.style.left = '0';
+ 		canvas.style.zIndex = '1';
+ 		canvas.style.pointerEvents = 'none';
+ 	}
+
+    lastTime = 0;
+    shakeTime = 0;
+    shakeTimeMax = 0;
+    shakeIntensity = 5;
     loop = () => {
- 	// 	ctx.clearRect(0, 0, w, h);
-        //
- 	// 	// get the time past the previous frame
- 	// 	var current_time = new Date().getTime();
-        // var last_time;
- 	// 	if (!lastTime) last_time = current_time;
- 	// 	var dt = (current_time - lastTime) / 1000;
- 	// 	lastTime = current_time;
-        //
- 	// 	if (shakeTime > 0) {
- 	// 		shakeTime -= dt;
- 	// 		var magnitude = (shakeTime / shakeTimeMax) * shakeIntensity;
- 	// 		var shakeX = random(-magnitude, magnitude);
- 	// 		var shakeY = random(-magnitude, magnitude);
- 	// 		cmNode.style.transform = 'translate(' + shakeX + 'px,' + shakeY + 'px)';
- 	// 	}
- 	// 	drawParticles();
- 	// 	requestAnimationFrame(loop);
+        if(this.isUnmounted) return;
+
+ 		this.ctx.clearRect(0,0,this.canvas().clientHeight,this.canvas().clientWidth);
+
+ 		// get the time past the previous frame
+ 		var current_time = new Date().getTime();
+        var last_time;
+ 		if (!this.lastTime) last_time = current_time;
+ 		var dt = (current_time - this.lastTime) / 1000;
+ 		this.lastTime = current_time;
+
+ 		if (this.shakeTime > 0) {
+ 			this.shakeTime -= dt;
+ 			var magnitude = (this.shakeTime / this.shakeTimeMax) * this.shakeIntensity;
+ 			var shakeX = random(-magnitude, magnitude);
+ 			var shakeY = random(-magnitude, magnitude);
+ 			this.props.cm.getWrapperElement().style.transform = 'translate(' + shakeX + 'px,' + shakeY + 'px)';
+ 		}
+ 		this.drawParticles();
+ 		requestAnimationFrame(this.loop);
+ 	}
+
+    particles:Particle[] = [];
+    effect = 1; // or 2
+    drawParticles = (timeDelta?:number) => {
+ 		var particle;
+ 		for (var i = this.particles.length; i--;) {
+ 			particle = this.particles[i];
+ 			if (!particle || particle.alpha < 0.01 || particle.size <= 0.5) { continue; }
+
+ 			if (this.effect === 1) { this.effect1(particle); }
+ 			else if (this.effect === 2) { this.effect2(particle); }
+ 		}
+ 	}
+
+    PARTICLE_GRAVITY = 0.08;
+    PARTICLE_ALPHA_FADEOUT = 0.96;
+    effect1(particle:Particle) {
+ 		particle.vy += this.PARTICLE_GRAVITY;
+ 		particle.x += particle.vx;
+ 		particle.y += particle.vy;
+ 		particle.alpha *= this.PARTICLE_ALPHA_FADEOUT;
+
+ 		this.ctx.fillStyle = 'rgba('+ particle.color[0] +','+ particle.color[1] +','+ particle.color[2] + ',' + particle.alpha + ')';
+ 		this.ctx.fillRect(Math.round(particle.x - 1), Math.round(particle.y - 1), particle.size, particle.size);
+ 	}
+
+ 	// Effect based on Soulwire's demo: http://codepen.io/soulwire/pen/foktm
+ 	effect2(particle:Particle) {
+ 		particle.x += particle.vx;
+ 		particle.y += particle.vy;
+ 		particle.vx *= particle.drag;
+ 		particle.vy *= particle.drag;
+ 		particle.theta += random( -0.5, 0.5 );
+ 		particle.vx += Math.sin( particle.theta ) * 0.1;
+ 		particle.vy += Math.cos( particle.theta ) * 0.1;
+ 		particle.size *= 0.96;
+
+        this.ctx.fillStyle = 'rgba('+ particle.color[0] +','+ particle.color[1] +','+ particle.color[2] + ',' + particle.alpha + ')';
+ 		this.ctx.beginPath();
+ 		this.ctx.arc(Math.round(particle.x - 1), Math.round(particle.y - 1), particle.size, 0, 2 * Math.PI);
+ 		this.ctx.fill();
+ 	}
+
+    throttledShake = throttle((time) => {
+ 		this.shakeTime = this.shakeTimeMax = time;
+ 	}, 100);
+
+    PARTICLE_NUM_RANGE = { min: 5, max: 10 };
+    particlePointer = 0;
+    MAX_PARTICLES = 500;
+    throttledSpawnParticles = throttle((type) => {
+        let cm = this.props.cm;
+
+        var cursorPos = cm.getDoc().getCursor();
+        var pos = cm.cursorCoords(cursorPos,'window');
+        var node = document.elementFromPoint(pos.left - 5, pos.top + 5);
+        type = cm.getTokenAt(cursorPos);
+        if (type) { type = type.type; };
+        var numParticles = random(this.PARTICLE_NUM_RANGE.min, this.PARTICLE_NUM_RANGE.max);
+        let color = getRGBComponents(node);
+        for (var i = numParticles; i--;) {
+            this.particles[this.particlePointer] = this.createParticle(pos.left + 10, pos.top, color);
+            this.particlePointer = (this.particlePointer + 1) % this.MAX_PARTICLES;
+        }
+    }, 100);
+
+    PARTICLE_VELOCITY_RANGE = {
+        x: [-1, 1],
+        y: [-3.5, -1.5]
+    }
+    createParticle(x, y, color) {
+ 		var p = {
+ 			x: x,
+ 			y: y + 10,
+ 			alpha: 1,
+ 			color: color,
+
+            // modifed below
+            drag: 0,
+            wander: 0,
+            theta: 0,
+            size: 0,
+            vx: 0,
+            vy: 0,
+ 		};
+ 		if (this.effect === 1) {
+ 			p.size = random(2, 4);
+ 			p.vx = this.PARTICLE_VELOCITY_RANGE.x[0] + Math.random() *
+ 					(this.PARTICLE_VELOCITY_RANGE.x[1] - this.PARTICLE_VELOCITY_RANGE.x[0]);
+ 			p.vy = this.PARTICLE_VELOCITY_RANGE.y[0] + Math.random() *
+ 					(this.PARTICLE_VELOCITY_RANGE.y[1] - this.PARTICLE_VELOCITY_RANGE.y[0]);
+ 		} else if (this.effect === 2) {
+ 			p.size = random(2, 8);
+ 			p.drag = 0.92;
+ 			p.vx = random(-3, 3);
+ 			p.vy = random(-3, 3);
+ 			p.wander = 0.15;
+ 			p.theta = random(0, 360) * Math.PI / 180;
+ 		}
+ 		return p;
  	}
 
     handleChange = () => {
-
+        this.throttledShake(0.3);
+        this.throttledSpawnParticles();
     };
+}
+
+interface Particle {
+    x: number;
+    y: number;
+    alpha: number;
+    color: [string,string,string];
+
+    // modifed below
+    drag?: number;
+    wander?: number;
+    theta?: number;
+    size?: number;
+    vx?: number;
+    vy?: number;
+}
+
+/** Get a random number in the min-max range, inclusive */
+function random(min:number, max:number) {
+    if (!max) { max = min; min = 0; }
+    return min + ~~(Math.random() * (max - min + 1))
+}
+
+/** Throttles the callback */
+function throttle (callback, limit) {
+    var wait = false;
+    return function (...args) {
+        if (!wait) {
+            callback.apply(this, args);
+            wait = true;
+            setTimeout(function () {
+                wait = false;
+            }, limit);
+        }
+    }
+}
+
+/** Get the colors of the html node */
+function getRGBComponents(node: Element): [string, string, string] {
+    if (node) {
+        try {
+            var color = getComputedStyle(node).color;
+            return color.match(/(\d+), (\d+), (\d+)/).slice(1) as any;
+        } catch(e) {
+            return ['255', '255', '255'];
+        }
+    } else {
+        return ['255', '255', '255'];
+    }
 }
 
  ;(function () {
