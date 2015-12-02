b4d460c9d047e4c99382f71f772a69a0641e8f10
basarat <basaratali@gmail.com>
2015-12-02 18:31:42 +1100

    WIP blaster

--- src/app/codemirror/addons/blaster.tsx
+++ src/app/codemirror/addons/blaster.tsx
@@ -1,10 +1,10 @@
 /**
  * Based on https://github.com/chinchang/code-blast-codemirror
  */
- /*
- Based on Joel Besada's lovely experiment
- https://twitter.com/JoelBesada/status/670343885655293952
-  */
+/*
+Based on Joel Besada's lovely experiment
+https://twitter.com/JoelBesada/status/670343885655293952
+ */
 
 import CodeMirror = require('codemirror');
 import ui = require('../../ui');
@@ -15,17 +15,17 @@ interface Props {
     cm: Editor;
 }
 
-export class Blaster extends ui.BaseComponent<Props,any>{
+export class Blaster extends ui.BaseComponent<Props, any>{
 
-    canvas = ():HTMLCanvasElement => this.refs['canvas'] as any;
-    render(){
-        let style = this.props.cm?{}:{display:'none'};
+    canvas = (): HTMLCanvasElement => this.refs['canvas'] as any;
+    render() {
+        let style = this.props.cm ? {} : { display: 'none' };
 
         return <canvas style={style} ref="canvas" />
     }
 
-    componentWillReceiveProps(props:Props,oldProps:Props){
-        if (!oldProps.cm && props.cm){
+    componentWillReceiveProps(props: Props, oldProps: Props) {
+        if (!oldProps.cm && props.cm) {
             props.cm.on('change', this.handleChange);
             this.initCanvas();
             this.loop();
@@ -37,104 +37,104 @@ export class Blaster extends ui.BaseComponent<Props,any>{
         this.props.cm.off('change', this.handleChange);
     }
 
-    ctx:CanvasRenderingContext2D;
+    ctx: CanvasRenderingContext2D;
     initCanvas = () => {
         let canvas = this.canvas();
- 		this.ctx = canvas.getContext('2d'),
+        this.ctx = canvas.getContext('2d'),
 
- 		canvas.style.position = 'absolute';
- 		canvas.style.width = "100%";
- 		canvas.style.height = "100%";
- 		canvas.style.zIndex = '1';
- 		canvas.style.pointerEvents = 'none';
- 	}
+            canvas.style.position = 'absolute';
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+        canvas.style.zIndex = '1';
+        canvas.style.pointerEvents = 'none';
+    }
 
     lastTime = 0;
     shakeTime = 0;
     shakeTimeMax = 0;
     shakeIntensity = 5;
     loop = () => {
-        if(this.isUnmounted) return;
+        if (this.isUnmounted) return;
 
         this.canvas().width = this.canvas().clientWidth;
         this.canvas().height = this.canvas().clientHeight;
-        this.ctx.clearRect(0,0,this.canvas().clientHeight,this.canvas().clientWidth);
+        this.ctx.clearRect(0, 0, this.canvas().clientHeight, this.canvas().clientWidth);
 
- 		// get the time past the previous frame
- 		var current_time = new Date().getTime();
+        // get the time past the previous frame
+        var current_time = new Date().getTime();
         var last_time;
- 		if (!this.lastTime) last_time = current_time;
- 		var dt = (current_time - this.lastTime) / 1000;
- 		this.lastTime = current_time;
+        if (!this.lastTime) last_time = current_time;
+        var dt = (current_time - this.lastTime) / 1000;
+        this.lastTime = current_time;
 
- 		if (this.shakeTime > 0) {
- 			this.shakeTime -= dt;
- 			var magnitude = (this.shakeTime / this.shakeTimeMax) * this.shakeIntensity;
- 			var shakeX = random(-magnitude, magnitude);
- 			var shakeY = random(-magnitude, magnitude);
- 			this.props.cm.getWrapperElement().style.transform = 'translate(' + shakeX + 'px,' + shakeY + 'px)';
- 		}
- 		this.drawParticles();
- 		requestAnimationFrame(this.loop);
- 	}
+        if (this.shakeTime > 0) {
+            this.shakeTime -= dt;
+            var magnitude = (this.shakeTime / this.shakeTimeMax) * this.shakeIntensity;
+            var shakeX = random(-magnitude, magnitude);
+            var shakeY = random(-magnitude, magnitude);
+            this.props.cm.getWrapperElement().style.transform = 'translate(' + shakeX + 'px,' + shakeY + 'px)';
+        }
+        this.drawParticles();
+        requestAnimationFrame(this.loop);
+    }
 
-    particles:Particle[] = [];
+    particles: Particle[] = [];
     effect = 2; // 1 or 2
-    drawParticles = (timeDelta?:number) => {
+    drawParticles = (timeDelta?: number) => {
         for (let particle of this.particles) {
             if (!particle || particle.alpha < 0.01 || particle.size <= 0.5) { continue; }
 
- 			if (this.effect === 1) { this.effect1(particle); }
- 			else if (this.effect === 2) { this.effect2(particle); }
+            if (this.effect === 1) { this.effect1(particle); }
+            else if (this.effect === 2) { this.effect2(particle); }
         }
- 	}
+    }
 
     PARTICLE_GRAVITY = 0.08;
     PARTICLE_ALPHA_FADEOUT = 0.96;
-    effect1(particle:Particle) {
- 		particle.vy += this.PARTICLE_GRAVITY;
- 		particle.x += particle.vx;
- 		particle.y += particle.vy;
- 		particle.alpha *= this.PARTICLE_ALPHA_FADEOUT;
+    effect1(particle: Particle) {
+        particle.vy += this.PARTICLE_GRAVITY;
+        particle.x += particle.vx;
+        particle.y += particle.vy;
+        particle.alpha *= this.PARTICLE_ALPHA_FADEOUT;
 
- 		this.ctx.fillStyle = 'rgba('+ particle.color[0] +','+ particle.color[1] +','+ particle.color[2] + ',' + particle.alpha + ')';
- 		this.ctx.fillRect(Math.round(particle.x - 1), Math.round(particle.y - 1), particle.size, particle.size);
- 	}
+        this.ctx.fillStyle = 'rgba(' + particle.color[0] + ',' + particle.color[1] + ',' + particle.color[2] + ',' + particle.alpha + ')';
+        this.ctx.fillRect(Math.round(particle.x - 1), Math.round(particle.y - 1), particle.size, particle.size);
+    }
 
- 	// Effect based on Soulwire's demo: http://codepen.io/soulwire/pen/foktm
- 	effect2(particle:Particle) {
- 		particle.x += particle.vx;
- 		particle.y += particle.vy;
- 		particle.vx *= particle.drag;
- 		particle.vy *= particle.drag;
- 		particle.theta += random( -0.5, 0.5 );
- 		particle.vx += Math.sin( particle.theta ) * 0.1;
- 		particle.vy += Math.cos( particle.theta ) * 0.1;
- 		particle.size *= 0.96;
+    // Effect based on Soulwire's demo: http://codepen.io/soulwire/pen/foktm
+    effect2(particle: Particle) {
+        particle.x += particle.vx;
+        particle.y += particle.vy;
+        particle.vx *= particle.drag;
+        particle.vy *= particle.drag;
+        particle.theta += random(-0.5, 0.5);
+        particle.vx += Math.sin(particle.theta) * 0.1;
+        particle.vy += Math.cos(particle.theta) * 0.1;
+        particle.size *= 0.96;
 
-        this.ctx.fillStyle = 'rgba('+ particle.color[0] +','+ particle.color[1] +','+ particle.color[2] + ',' + particle.alpha + ')';
- 		this.ctx.beginPath();
- 		this.ctx.arc(Math.round(particle.x - 1), Math.round(particle.y - 1), particle.size, 0, 2 * Math.PI);
- 		this.ctx.fill();
- 	}
+        this.ctx.fillStyle = 'rgba(' + particle.color[0] + ',' + particle.color[1] + ',' + particle.color[2] + ',' + particle.alpha + ')';
+        this.ctx.beginPath();
+        this.ctx.arc(Math.round(particle.x - 1), Math.round(particle.y - 1), particle.size, 0, 2 * Math.PI);
+        this.ctx.fill();
+    }
 
     throttledShake = throttle((time) => {
- 		this.shakeTime = this.shakeTimeMax = time;
- 	}, 100);
+        this.shakeTime = this.shakeTimeMax = time;
+    }, 100);
 
     PARTICLE_NUM_RANGE = { min: 5, max: 10 };
     throttledSpawnParticles = throttle((type) => {
         let cm = this.props.cm;
         var cursorPos = cm.getDoc().getCursor();
 
-        let posForNode = cm.cursorCoords(cursorPos,'window');
+        let posForNode = cm.cursorCoords(cursorPos, 'window');
         var node = document.elementFromPoint(posForNode.left - 5, posForNode.top + 5);
 
         type = cm.getTokenAt(cursorPos);
         if (type) { type = type.type; };
         var numParticles = random(this.PARTICLE_NUM_RANGE.min, this.PARTICLE_NUM_RANGE.max);
         let color = getRGBComponents(node);
-        let pos = cm.cursorCoords(cursorPos,'page');
+        let pos = cm.cursorCoords(cursorPos, 'page');
 
         for (var i = numParticles; i--;) {
             this.particles[i] = this.createParticle(pos.left + 10, pos.top, color);
@@ -145,12 +145,12 @@ export class Blaster extends ui.BaseComponent<Props,any>{
         x: [-1, 1],
         y: [-3.5, -1.5]
     }
-    createParticle(x:number, y: number, color:[string,string,string]) {
- 		var p = {
- 			x: x,
- 			y: y + 10,
- 			alpha: 1,
- 			color: color,
+    createParticle(x: number, y: number, color: [string, string, string]) {
+        var p = {
+            x: x,
+            y: y + 10,
+            alpha: 1,
+            color: color,
 
             // modifed below
             drag: 0,
@@ -159,23 +159,23 @@ export class Blaster extends ui.BaseComponent<Props,any>{
             size: 0,
             vx: 0,
             vy: 0,
- 		};
- 		if (this.effect === 1) {
- 			p.size = random(2, 4);
- 			p.vx = this.PARTICLE_VELOCITY_RANGE.x[0] + Math.random() *
- 					(this.PARTICLE_VELOCITY_RANGE.x[1] - this.PARTICLE_VELOCITY_RANGE.x[0]);
- 			p.vy = this.PARTICLE_VELOCITY_RANGE.y[0] + Math.random() *
- 					(this.PARTICLE_VELOCITY_RANGE.y[1] - this.PARTICLE_VELOCITY_RANGE.y[0]);
- 		} else if (this.effect === 2) {
- 			p.size = random(2, 8);
- 			p.drag = 0.92;
- 			p.vx = random(-3, 3);
- 			p.vy = random(-3, 3);
- 			p.wander = 0.15;
- 			p.theta = random(0, 360) * Math.PI / 180;
- 		}
- 		return p;
- 	}
+        };
+        if (this.effect === 1) {
+            p.size = random(2, 4);
+            p.vx = this.PARTICLE_VELOCITY_RANGE.x[0] + Math.random() *
+                (this.PARTICLE_VELOCITY_RANGE.x[1] - this.PARTICLE_VELOCITY_RANGE.x[0]);
+            p.vy = this.PARTICLE_VELOCITY_RANGE.y[0] + Math.random() *
+                (this.PARTICLE_VELOCITY_RANGE.y[1] - this.PARTICLE_VELOCITY_RANGE.y[0]);
+        } else if (this.effect === 2) {
+            p.size = random(2, 8);
+            p.drag = 0.92;
+            p.vx = random(-3, 3);
+            p.vy = random(-3, 3);
+            p.wander = 0.15;
+            p.theta = random(0, 360) * Math.PI / 180;
+        }
+        return p;
+    }
 
     handleChange = () => {
         this.throttledShake(0.3);
@@ -187,7 +187,7 @@ interface Particle {
     x: number;
     y: number;
     alpha: number;
-    color: [string,string,string];
+    color: [string, string, string];
 
     // modifed below
     drag?: number;
@@ -199,19 +199,19 @@ interface Particle {
 }
 
 /** Get a random number in the min-max range, inclusive */
-function random(min:number, max:number) {
+function random(min: number, max: number) {
     if (!max) { max = min; min = 0; }
     return min + ~~(Math.random() * (max - min + 1))
 }
 
 /** Throttles the callback */
-function throttle (callback, limit) {
+function throttle(callback, limit) {
     var wait = false;
-    return function (...args) {
+    return function(...args) {
         if (!wait) {
             callback.apply(this, args);
             wait = true;
-            setTimeout(function () {
+            setTimeout(function() {
                 wait = false;
             }, limit);
         }
@@ -224,7 +224,7 @@ function getRGBComponents(node: Element): [string, string, string] {
         try {
             var color = getComputedStyle(node).color;
             return color.match(/(\d+), (\d+), (\d+)/).slice(1) as any;
-        } catch(e) {
+        } catch (e) {
             return ['255', '255', '255'];
         }
     } else {
