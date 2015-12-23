47531bc96ec06e5d1e6e21bbaee0759be8cf7e0c
Basarat <basaratali@gmail.com>
2015-12-23 18:38:16 +1100

    :lipstick:

--- src/app/clipboard.tsx
+++ src/app/clipboard.tsx
@@ -14,7 +14,7 @@ export interface State {
 let buttonStyle = {
     height: '18px',
     padding: '2px 3px',
-    display: 'inline-block',
+    display: 'inline-flex',
     cursor: 'pointer',
     backgroundImage: 'linear-gradient(#7B7B7B, #353434)',
     border: '1px solid #464646',
@@ -29,14 +29,14 @@ let buttonStyle = {
 
 let clippy = {
     width: '12px',
-    height: '10px'
+    height: '12px'
 }
 
 @ui.Radium
 export class Clipboard extends BaseComponent<Props, State>{
     render(){
         return (
-            <button style={buttonStyle} data-clipboard-text={this.props.text} onClick={(event)=>event.stopPropagation()}>
+            <button style={csx.extend(buttonStyle,csx.center)} data-clipboard-text={this.props.text} onClick={(event)=>event.stopPropagation()}>
                 <img src="assets/clippy.svg" style={clippy}/>
             </button>
         );
