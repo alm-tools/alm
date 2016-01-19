6fea66ce8b0ca82ca611de3fc20b6dd300ba815c
Basarat <basaratali@gmail.com>
2016-01-19 17:59:10 +1100

    Only allow one instance of farming

--- src/server/lang/modules/findAndReplaceMultiService.ts
+++ src/server/lang/modules/findAndReplaceMultiService.ts
@@ -10,26 +10,53 @@ interface FarmConfig {
     globs: string[];
 }
 
+interface FarmingState {
+    ignore: () => void;
+}
+
+let farmState:FarmingState = null;
+
 /**
- * TODO: move to observables that can be:
- * - cancelled
- * - streamed
- * - collected to give initial value
+ * Only allows one active process of farming
  */
-let farmCommand = (cfg: FarmConfig): Promise<string> => {
-    return new Promise((resolve, reject) => {
-        /**
-         * https://git-scm.com/docs/git-grep
-         * 
-         * n: line number
-         * E: extended regexp
-         * w: Match the pattern only at word boundary (also takes into account new lines 💟)
-         */
-        cp.exec(`git grep -En -- ${cfg.globs.join(' ')}`, { cwd: wd.getProjectRoot() }, (err, stdout, stderr) => {
-            if (stderr.toString().trim().length) {
-                return resolve(stderr.toString());
-            }
-            return resolve(stdout);
-        });
+let restartFarming = (cfg: FarmConfig) => {
+    if (farmState){
+        farmState.ignore();
+        farmState = null;
+    }
+
+    /** Allows us to abort a search */
+    let ignored = false;
+    const ignore = () => ignored = true;
+
+    /**
+     * https://git-scm.com/docs/git-grep
+     *
+     * // General main ones
+     * n: line number
+     * E: extended regexp
+     * I: don't match binary files
+     *
+     * // Useful toggles
+     * w: Match the pattern only at word boundary (also takes into account new lines 💟)
+     * i: ignore case
+     */
+    const grep = cp.spawn(`git grep -EIn -- ${cfg.globs.join(' ')}`);
+
+    grep.stdout.on('data', (data) => {
+        if (ignored) return;
+        console.log(`stdout: ${data}`);
     });
+
+    grep.stderr.on('data', (data) => {
+        if (ignored) return;
+        console.log(`stderr: ${data}`);
+    });
+
+    grep.on('close', (code) => {
+        if (ignored) return;
+        console.log(`child process exited with code ${code}`);
+    });
+
+    farmState = {ignore};
 }
