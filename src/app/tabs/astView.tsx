7d295b3afe19bd307c00bd25ff792056ff70a9e9
Basarat <basaratali@gmail.com>
2016-02-02 08:47:42 +1100

    AST VIEW more :rose:

--- src/app/tabs/astView.tsx
+++ src/app/tabs/astView.tsx
@@ -58,23 +58,33 @@ export class ASTView extends ui.BaseComponent<Props, State> implements tab.Compo
     mode: Types.ASTMode;
     astViewRenderer : ASTViewRenderer;
     componentDidMount() {
-        server.openFile({filePath:this.filePath}).then(res=>{
-            this.setState({text: res.contents});
-        });
-
-        server.getAST({mode:this.mode,filePath:this.filePath})
-            .then((res)=>{
-                this.astViewRenderer = new ASTViewRenderer({
-                    rootNode: res.root,
-                    _mainContent: $(this.refs.graphRoot),
-                    display: this.display
-                })
-            })
-            .catch((err)=>{
-                this.setState({
-                    error: `Failed to get the AST details for the file ${this.filePath}. Make sure it is in the active project. Change project using Alt+Shift+P.`
-                });
+        const reloadData = () => {
+            this.setState({ error: null });
+            server.openFile({filePath:this.filePath}).then(res=>{
+                this.setState({text: res.contents});
             });
+
+            server.getAST({mode:this.mode,filePath:this.filePath})
+                .then((res)=>{
+                    this.astViewRenderer = new ASTViewRenderer({
+                        rootNode: res.root,
+                        _mainContent: $(this.refs.graphRoot),
+                        display: this.display
+                    })
+                })
+                .catch((err)=>{
+                    this.setState({
+                        error: `Failed to get the AST details for the file ${this.filePath}. Make sure it is in the active project. Change project using Alt+Shift+P.`
+                    });
+                });
+        }
+
+        reloadData();
+        this.disposible.add(
+            cast.activeProjectConfigDetailsUpdated.on(()=>{
+                reloadData();
+            })
+        );
     }
 
     render() {
