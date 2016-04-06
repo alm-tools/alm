19bf421d8f1b66647d908f4e97986b6698e4ee70
basarat <basaratali@gmail.com>
2016-04-06 21:23:31 +1000

    going back on master soon

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,6 +1,6 @@
 #Resume work on a new machine
 
 git -c diff.mnemonicprefix=false -c core.quotepath=false fetch origin
-git -c diff.mnemonicprefix=false -c core.quotepath=false pull --rebase origin bas/async
+git -c diff.mnemonicprefix=false -c core.quotepath=false pull --rebase origin master
 
 npm install
