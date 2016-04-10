5f2ab9a288439679df9e71ae2b3f20918cd7ad35
basarat <basaratali@gmail.com>
2016-04-10 17:24:20 +1000

    cleanup templates

--- src/app/codemirror/addons/autocomplete/templates.tsx
+++ src/app/codemirror/addons/autocomplete/templates.tsx
@@ -47,7 +47,6 @@ type ParsedToken = string | {
     /**
      * only one of these really. But its more hasel to split it into `|` descriminators
      */
-    line_selection?: boolean;
     cursor?: boolean;
     variable?: ParsedVariable;
 }
@@ -131,9 +130,6 @@ function goIntoInsertMode(cm: CodeMirror.Editor) {
 //       Variable token, to be populated by the user.
 //   { cursor: true }
 //       The cursor will be placed here after completing the template
-//   { line_separator: true }
-//       If the template surrounds existing text, the existing text will be
-//       placed here. Not implemented currently.
 export class Template {
     public name: any;
     public description: any;
@@ -221,7 +217,7 @@ export class Template {
             } else if (token.cursor) {
                 cursor = Pos(line, col);
             } else {
-                // Unhandled tokens, e.g. line_selection. Ignore.
+                throw new Error('Unknown in Template:' + JSON.stringify(token));
             }
         }
 
@@ -295,10 +291,6 @@ function parseTemplate(content: string): ParsedToken[] {
                     if (token == 'cursor') {
                         tokens.push({
                             cursor: true
-                        });
-                    } else if (token == 'line_selection') {
-                        tokens.push({
-                            line_selection: true
                         });
                     } else {
                         let tokenSplit = token.split(':');
@@ -567,7 +559,7 @@ var templates: TemplatesForContext = {
         {
             "name": "for",
             "description": "iterate over array",
-            "template": "for (var ${2:index} = 0; ${index} < ${1:array}.length; ${index}++) {\n\t${line_selection}${cursor}\n}"
+            "template": "for (var ${2:index} = 0; ${index} < ${1:array}.length; ${index}++) {\n\t${cursor}\n}"
         },
         {
             "name": "for",
@@ -584,27 +576,23 @@ var templates: TemplatesForContext = {
             "description": "iterate using for .. of",
             "template": "for (var ${2:iterable_element} of ${1:iterable}) {\n\t${cursor}\n}"
         },
-        { "name": "do", "description": "do while statement", "template": "do {\n\t${line_selection}${cursor}\n} while (${condition});" },
+        { "name": "do", "description": "do while statement", "template": "do {\n\t${cursor}\n} while (${condition});" },
         { "name": "switch", "description": "switch case statement", "template": "switch (${key}) {\n\tcase ${value}:\n\t\t${cursor}\n\t\tbreak;\n\n\tdefault:\n\t\tbreak;\n}" },
-        { "name": "if", "description": "if statement", "template": "if (${condition}) {\n\t${line_selection}${cursor}\n}" },
+        { "name": "if", "description": "if statement", "template": "if (${condition}) {\n\t${cursor}\n}" },
         { "name": "ifelse", "description": "if else statement", "template": "if (${condition}) {\n\t${cursor}\n} else {\n\t\n}" },
         { "name": "elseif", "description": "else if block", "template": "else if (${condition}) {\n\t${cursor}\n}" },
         { "name": "else", "description": "else block", "template": "else {\n\t${cursor}\n}" },
-        { "name": "try", "description": "try catch block", "template": "try {\n\t${line_selection}${cursor}\n} catch (e) {\n\t// ${todo}: handle exception\n}" },
+        { "name": "try", "description": "try catch block", "template": "try {\n\t${cursor}\n} catch (e) {\n\t// ${todo}: handle exception\n}" },
         { "name": "catch", "description": "catch block", "template": "catch (e) {\n\t${cursor}// ${todo}: handle exception\n}" },
         { "name": "function", "description": "function", "template": "function ${name}(${}) {\n\t${cursor}\n}" },
         { "name": "function", "description": "anonymous function", "template": "function (${}) {\n\t${cursor}\n}" },
         { "name": "new", "description": "create new object", "template": "var ${name} = new ${type}(${arguments});" },
         { "name": "lazy", "description": "lazy creation", "template": "if (${name:var} == null) {\n\t${name} = new ${type}(${arguments});\n\t${cursor}\n}\n\nreturn ${name};" },
-        { "name": "<code>", "description": "<code></code>", "template": "<code>${word_selection}${}</code>${cursor}" },
         { "name": "null", "description": "<code>null</code>", "template": "<code>null</code>" },
         { "name": "true", "description": "<code>true</code>", "template": "<code>true</code>" },
         { "name": "false", "description": "<code>false</code>", "template": "<code>false</code>" },
-        { "name": "<pre>", "description": "<pre></pre>", "template": "<pre>${word_selection}${}</pre>${cursor}" },
-        { "name": "<b>", "description": "<b></b>", "template": "<b>${word_selection}${}</b>${cursor}" },
-        { "name": "<i>", "description": "<i></i>", "template": "<i>${word_selection}${}</i>${cursor}" },
         { "name": "@author", "description": "author name", "template": "@author ${user}" },
-        { "name": "while", "description": "while loop with condition", "template": "while (${condition}) {\n\t${line_selection}${cursor}\n}" }
+        { "name": "while", "description": "while loop with condition", "template": "while (${condition}) {\n\t${cursor}\n}" }
     ]
 };
 addTemplates(templates);
