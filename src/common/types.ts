59e74d19bf5ce1afc4c0e189210ad0ff517a2ae7
basarat <basaratali@gmail.com>
2016-04-10 16:20:16 +1000

    kind

--- src/common/types.ts
+++ src/common/types.ts
@@ -137,7 +137,7 @@ export interface Completion {
     name?: string; // stuff like "toString"
     kind?: string; // stuff like "var"
     comment?: string; // the docComment if any
-    display?: string; // This is either displayParts (for functions) or just the kind duplicated
+    display?: string; // This is displayParts (for functions)
 
     /** If snippet is specified then the above stuff is ignored */
     snippet?: string;
