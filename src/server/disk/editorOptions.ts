222f386d8ba2150937ac0b6be30487effa895698
Basarat <basaratali@gmail.com>
2016-04-12 11:45:16 +1000

    WIP editorconfig parsing complete

--- src/server/disk/editorOptions.ts
+++ src/server/disk/editorOptions.ts
@@ -10,20 +10,31 @@ export function getEditorOptions(filePath: string): EditorOptions {
 
     // console.log(filePath, config); // DEBUG
 
-    // Convert editorconfig to EditorOptions
+    /**
+     * Convert editorconfig to EditorOptions
+     */
+
+    // CR sounds like a dumb option. I am treating it as same as os.EOL
+    const end_of_line = config.end_of_line || 'os';
     const newLineCharacter =
-        config.end_of_line && config.end_of_line === 'lf' ? '\n'
-        : config.end_of_line && config.end_of_line === 'crlf' ? '\r\n'
+        end_of_line && config.end_of_line === 'lf' ? '\n'
+        : end_of_line && config.end_of_line === 'crlf' ? '\r\n'
         : os.EOL;
 
     const indentSize =
         config.indent_size || 4;
 
+    const tabSize =
+        config.tab_width || 4;
+
+    const indent_style = config.indent_style || 'space';
+    const convertTabsToSpaces =
+        indent_style === 'space' ? true : false;
 
     return {
         indentSize,
-        tabSize: 4,
+        tabSize,
         newLineCharacter,
-        convertTabsToSpaces: true,
+        convertTabsToSpaces,
     }
 }
