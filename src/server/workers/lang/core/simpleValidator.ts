da9effe3743537aaf93b028085156578e0227746
basarat <basaratali@gmail.com>
2016-04-24 19:18:35 +1000

    WIP better validator

--- src/server/workers/lang/core/simpleValidator.ts
+++ src/server/workers/lang/core/simpleValidator.ts
@@ -2,17 +2,27 @@
 // But great for simple config validation
 // works only by "n" valid options
 
-export var types = {
-    string: 'string',
-    boolean: 'boolean',
-    number: 'number'
+type Type = 'string' | 'boolean' | 'number' | 'object' | 'array';
+
+export const types = {
+    string: 'string' as Type,
+    boolean: 'boolean' as Type,
+    number: 'number' as Type,
+    object: 'object' as Type,
+    array: 'array' as Type,
+}
+
+export interface MemberDefinition {
+    type: Type;
+    validValues?: string[];
+    /** Only used if `type` is `object` or `array` */
+    objectDefinition?: ValidationInfo;
 }
 
 export interface ValidationInfo {
-    [name: string]: {
-        validValues?: string[];
-        type?: string;
-    }
+    members: { [name: string]: MemberDefinition };
+    /** These are members that you know about but don't make sense to validate for you */
+    extraMembers?: string[];
 }
 
 export interface Errors {
@@ -24,9 +34,11 @@ export interface Errors {
 export class SimpleValidator {
 
     private potentialLowerCaseMatch: { [key: string]: string };
+    private extraMembers = [];
     constructor(public validationInfo: ValidationInfo) {
         this.potentialLowerCaseMatch = {};
         Object.keys(validationInfo).forEach(k=> this.potentialLowerCaseMatch[k.toLowerCase()] = k);
+        this.extraMembers = validationInfo.extraMembers || [];
     }
 
     validate = (config: any): Errors => {
@@ -38,21 +50,29 @@ export class SimpleValidator {
                 if (this.potentialLowerCaseMatch[k]) {
                     errors.extraKeys.push(`Key: '${k}' is a potential lower case match for '${this.potentialLowerCaseMatch[k]}'. Fix the casing.`);
                 }
+                else if (this.extraMembers.some(m => m === k)) {
+                    return; // continue. as we don't care about validating this key
+                }
                 else {
                     errors.extraKeys.push(`Unknown Option: ${k}`)
                 }
             }
             // Do validation
             else {
-                var validationInfo = this.validationInfo[k];
-                var value: any = config[k];
+                const validationInfo = this.validationInfo[k];
+                const value: any = config[k];
+                /** Do a valid values check */
                 if (validationInfo.validValues && validationInfo.validValues.length) {
                     var validValues = validationInfo.validValues;
                     if (!validValues.some(valid => valid.toLowerCase() === value.toLowerCase())) {
                         errors.invalidValues.push(`Key: '${k}' has an invalid value: ${value}`);
                     }
                 }
-                if (validationInfo.type && typeof value !== validationInfo.type) {
+                /** Do an array check */
+                if (validationInfo.type === 'array') {
+
+                }
+                else if (typeof value !== validationInfo.type) {
                     errors.invalidValues.push(`Key: '${k}' has an invalid type: ${typeof value}`)
                 }
             }
