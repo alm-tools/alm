1624d015abebdb82a249d14826a0e58fd3528067
basarat <basaratali@gmail.com>
2016-04-30 11:58:20 +1000

    fix: error panel now shows over golden layout

--- src/app/mainPanel.tsx
+++ src/app/mainPanel.tsx
@@ -124,7 +124,7 @@ export class MainPanel extends BaseComponent<Props, State>{
         }
 
         return (
-            <div>
+            <div style={{zIndex:6 /* force over golden-layout */, backgroundColor:'rgb(39, 40, 34)'}}>
                 <ui.VelocityTransitionGroup
                     enter={{animation: "slideDown", duration: 150}}
                     leave={{animation: "slideUp", duration: 150}}>
