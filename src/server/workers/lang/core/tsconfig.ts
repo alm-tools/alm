0a4af975b76cb9aff68e5f15b3cb0915dce11474
basarat <basaratali@gmail.com>
2016-05-08 12:31:08 +1000

    cannot use both `files` and `exclude` :rose:

--- src/server/workers/lang/core/tsconfig.ts
+++ src/server/workers/lang/core/tsconfig.ts
@@ -232,6 +232,11 @@ export function getProjectSync(pathOrSrcFile: string): GetProjectSyncResponse {
         let bland = json.parseErrorToCodeError(projectFilePath,res.error);
         return { error: bland };
     }
+    // Additional global level validations
+    if (projectSpec.files && projectSpec.exclude) {
+        let bland = makeBlandError(projectFilePath, 'You cannot use both "files" and "exclude" in tsconfig.json');
+        return { error: bland };
+    }
 
     // Setup default project options
     if (!projectSpec.compilerOptions) projectSpec.compilerOptions = {};
