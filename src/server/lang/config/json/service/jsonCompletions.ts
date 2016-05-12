0e9c8904bf2be84e2a1c0b528f7ded063742fab8
Basarat <basaratali@gmail.com>
2016-05-13 07:58:05 +1000

    WIP json completion

--- src/server/lang/config/json/service/jsonCompletions.ts
+++ src/server/lang/config/json/service/jsonCompletions.ts
@@ -2,4 +2,18 @@
  * Based on https://github.com/Microsoft/vscode/blob/master/extensions/json/server/src/jsonCompletion.ts
  *
  * But with our types etc.
+ *
+ * I deleted stuff around `jsonContributions` (I have no idea what it is)
  */
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import Parser = require('../jsonParser');
+import SchemaService = require('./jsonSchemaService');
+import JsonSchema = require('../jsonSchema');
+import {localize} from "../localize";
+type Thenable<T> = Promise<T>;
