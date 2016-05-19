d367da881cf570d38cb8451dcbae10ff14fb0786
basarat <basaratali@gmail.com>
2016-05-19 14:27:27 +1000

    initial function creation

--- src/server/workers/lang/classDiagram/classDiagram.ts
+++ src/server/workers/lang/classDiagram/classDiagram.ts
@@ -3,3 +3,23 @@
  *
  * Based on the tsviz project https://github.com/joaompneves/tsviz 🌹
  */
+
+/** Imports */
+import * as tsAnalyzer from "./tsAnalyzer";
+import * as utils from "../../../../common/utils";
+
+/** We just use the *active* project if any */
+import * as activeProject from "../activeProject";
+let getProject = activeProject.GetProject.getCurrentIfAny;
+
+/**
+ * Get a uml diagram structure for a file
+ */
+export function getUmlDiagramForFile(query: { filePath: string }) : Promise<{}> {
+    let project = getProject();
+    const sourceFile = project.getProjectSourceFiles().find(f => f.fileName === query.filePath);
+
+    const modules = tsAnalyzer.collectInformation(project.languageService.getProgram(), sourceFile);
+
+    return utils.resolve({});
+}
