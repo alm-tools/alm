089030e08e02f51850142b52a92ad6a2bdbc4920
Basarat <basaratali@gmail.com>
2016-05-24 19:52:18 +1000

    WIP json hover

--- src/server/lang/config/jsonHover.ts
+++ src/server/lang/config/jsonHover.ts
@@ -12,9 +12,27 @@
  * ```
  * Both `node` and `location` are extremely handy
  */
+import Parser = require('./json/jsonParser');
+
 
 import * as utils from "../../../common/utils";
+import * as fmc from "../../disk/fileModelCache";
 export function getQuickInfo(query: { filePath: string, position: number }): Promise<{ comment: string }> {
     const comment = '';
+
+    const {filePath} = query;
+    const fileName = utils.getFileName(filePath).toLowerCase();
+    const offset = query.position;
+
+    const contents = fmc.getOrCreateOpenFile(filePath).getContents();
+    const doc = Parser.parse(contents);
+    let node = doc.getNodeFromOffsetEndInclusive(offset);
+    const location = node.getNodeLocation();
+
+    /** Provide latest version hint for depencencies */
+    if ((location.matches(['dependencies']) || location.matches(['devDependencies']) || location.matches(['optionalDependencies']) || location.matches(['peerDependencies']))) {
+        // TODO: make the xhr query
+    }
+
     return utils.resolve({ comment })
 }
