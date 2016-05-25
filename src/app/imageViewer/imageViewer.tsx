795cbedf6418d2f92f3c97059b7da6cdc0fb33e4
Basarat <basaratali@gmail.com>
2016-05-25 18:50:41 +1000

    image viewer refs https://github.com/alm-tools/alm/issues/106

--- src/app/imageViewer/imageViewer.tsx
+++ src/app/imageViewer/imageViewer.tsx
@@ -7,11 +7,33 @@ import * as React from "react";
 import * as gls from "../base/gls";
 import * as csx from "../../../node_modules/csx/csx";
 
+/**
+ * ViewerJS
+ * https://github.com/fengyuanchen/viewerjs
+ */
+const Viewer = require('viewerjs/dist/viewer');
+require('viewerjs/dist/viewer.css');
+require('./imageViewer.css');
+
+
 export class ImageViewer extends ui.BaseComponent<{ filePath: string, onClick: () => any }, {}>{
+    componentDidMount() {
+        const root = this.refs['img'];
+        var viewer = new Viewer(root, {
+            toolbar: true,
+            inline: true,
+            navbar: false,
+            button: false,
+            movable: false, // Because it conflicts with flex box centering
+        });
+        this.disposible.add({
+            dispose: () => viewer.destroy()
+        });
+    }
     render() {
         const {filePath} = this.props;
-        return <div style={csx.extend(csx.flex, csx.centerCenter)} onClick={this.props.onClick}>
-            <img style={{}} src={`${utils.imageUrl}/${filePath}`}/>
+        return <div style={csx.extend(csx.flex, csx.centerCenter) } onClick={this.props.onClick}>
+            <img ref="img" style={{opacity:0}} src={`${utils.imageUrl}/${filePath}`}/>
         </div>
     }
 }
