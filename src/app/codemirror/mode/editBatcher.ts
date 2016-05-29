b3ef5f17495b81341608dea5efd6166da172eb7c
Basarat <basaratali@gmail.com>
2016-05-30 09:21:00 +1000

    feat(batching) fronted now batches edits before sending to server :rose:

--- src/app/codemirror/mode/editBatcher.ts
+++ src/app/codemirror/mode/editBatcher.ts
@@ -3,6 +3,7 @@
  */
 /** imports */
 import {cast, server} from "../../../socket/socketClient";
+import * as utils from "../../../common/utils";
 
 
 /**
@@ -12,6 +13,26 @@ const pendingQueue: { [filePath: string]: CodeEdit[] } = Object.create(null);
 
 
 export function addToQueue(filePath: string, edit: CodeEdit) {
-    // TODO: batch by trottling (by filepath) and send these to the server
-    server.editFile({ filePath: filePath, edits: [edit] });
+    // Batch by by filePath and setup for sending later
+    if (!pendingQueue[filePath]) pendingQueue[filePath] = [];
+    pendingQueue[filePath].push(edit);
+
+    // Mark for sending
+    delayedFlushQueue();
 }
+
+export function flushQueue() {
+    Object.keys(pendingQueue).forEach(filePath => {
+        const edits = pendingQueue[filePath];
+        server.editFile({ filePath: filePath, edits });
+
+        /** Clear for future */
+        delete pendingQueue[filePath];
+    });
+}
+
+/**
+ * Note: this delay must be less than any other delays
+ * (e.g. autocomplete throttling should be more than this).
+ */
+export const delayedFlushQueue = utils.throttle(flushQueue, 200);
