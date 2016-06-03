1d19ca96b92fce37075a24717b35c41f56ba2c4b
basarat <basaratali@gmail.com>
2016-06-04 09:48:47 +1000

    doc cache : reload file if changed on disk :rose:

--- src/app/codemirror/mode/docCache.ts
+++ src/app/codemirror/mode/docCache.ts
@@ -133,7 +133,7 @@ function getOrCreateDoc(filePath: string): Promise<DocPromiseResult> {
             const doc = monaco.editor.createModel(res.contents, language);
             doc.filePath = filePath;
 
-            let editToIgnoreCount = 0;
+            let editCameFromServerCount = 0;
 
             /** This is used for monaco edit operation counting purposes */
             let editorOperationCounter = 0;
@@ -146,8 +146,8 @@ function getOrCreateDoc(filePath: string): Promise<DocPromiseResult> {
                 // if this edit is happening
                 // because *we edited it due to a server request*
                 // we should exit
-                if (editToIgnoreCount) {
-                    editToIgnoreCount--;
+                if (editCameFromServerCount) {
+                    editCameFromServerCount--;
                     return;
                 }
                 let codeEdit: CodeEdit = {
@@ -190,89 +190,29 @@ function getOrCreateDoc(filePath: string): Promise<DocPromiseResult> {
                         }
 
                         /** Mark as ignoring before applying the edit */
-                        editToIgnoreCount++;
+                        editCameFromServerCount++;
                         doc.pushEditOperations([], [editOperation], null);
                     }
                 });
             });
 
-            // TODO: mon
-            // // create the doc
-            // let doc = new CodeMirror.Doc(res.contents, mode);
-            // doc.filePath = filePath;
-            // doc.rootDoc = true;
-            //
-            // // setup to push doc changes to server
-            // (doc as any).on('change', (doc: CodeMirror.Doc, change: CodeMirror.EditorChange) => {
-            //     // console.log('sending server edit', sourceId)
-            //
-            //     if (change.origin == cameFromNetworkSourceId) {
-            //         return;
-            //     }
-            //
-            //     let codeEdit: CodeEdit = {
-            //         from: { line: change.from.line, ch: change.from.ch },
-            //         to: { line: change.to.line, ch: change.to.ch },
-            //         newText: change.text.join('\n'),
-            //         sourceId: localSourceId
-            //     };
-            //
-            //     // Send the edit
-            //     editBatcher.addToQueue(filePath, codeEdit);
-            //
-            //     // Keep the ouput status cache informed
-            //     state.ifJSStatusWasCurrentThenMoveToOutOfDate({inputFilePath: filePath});
-            // });
-            //
-            // // setup to get doc changes from server
-            // cast.didEdits.on(res=> {
-            //
-            //     // console.log('got server edit', res.edit.sourceId,'our', sourceId)
-            //
-            //     let codeEdits = res.edits;
-            //
-            //     codeEdits.forEach(codeEdit => {
-            //         if (res.filePath == filePath && codeEdit.sourceId !== localSourceId) {
-            //             // Keep the classifier in sync
-            //             if (isTsFile) { classifierCache.editFile(filePath, codeEdit); }
-            //
-            //             // Note that we use *mark as coming from server* so we don't go into doc.change handler later on :)
-            //             doc.replaceRange(codeEdit.newText, codeEdit.from, codeEdit.to, cameFromNetworkSourceId);
-            //         }
-            //     });
-            // });
-            //
-            // // setup loading saved files changing on disk
-            // cast.savedFileChangedOnDisk.on((res) => {
-            //     if (res.filePath == filePath
-            //         && doc.getValue() !== res.contents) {
-            //
-            //         // Keep the classifier in sync
-            //         if (isTsFile) { classifierCache.setContents(filePath, res.contents); }
-            //
-            //         // preserve cursor for all linked docs
-            //         doc.iterLinkedDocs((linked) => {
-            //             if (linked.getEditor()) {
-            //                 const cursor = linked.getCursor();
-            //                 setTimeout(()=>{
-            //                     linked.setCursor(cursor);
-            //                 });
-            //             }
-            //         });
-            //
-            //         // preserve cursor
-            //         let cursor = doc.getCursor();
-            //
-            //         // Note that we use *mark as coming from server* so we don't go into doc.change handler later on :)
-            //         // Not using setValue as it doesn't take sourceId
-            //         let lastLine = doc.lastLine();
-            //         let lastCh = doc.getLine(lastLine).length;
-            //         doc.replaceRange(res.contents, { line: 0, ch: 0 }, { line: lastLine, ch: lastCh }, cameFromNetworkSourceId);
-            //
-            //         // restore cursor
-            //         doc.setCursor(cursor);
-            //     }
-            // })
+            // setup loading saved files changing on disk
+            cast.savedFileChangedOnDisk.on((res) => {
+                if (res.filePath == filePath
+                    && doc.getValue() !== res.contents) {
+
+                    // Keep the classifier in sync
+                    if (isTsFile) { classifierCache.setContents(filePath, res.contents); }
+
+                    // TODO: mon
+                    // preserve cursor
+
+                    // Note that we use *mark as coming from server* so we don't go into doc.change handler later on :)
+                    editCameFromServerCount++;
+
+                    doc.setValue(res.contents);
+                }
+            });
 
             // Finally return the doc
             return { doc, isTsFile, editorOptions: res.editorOptions };
