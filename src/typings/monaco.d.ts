c49ca862f6c90b9ca2351fc47f92027494aea513
basarat <basaratali@gmail.com>
2016-06-06 20:25:17 +1000

    plan

--- src/typings/monaco.d.ts
+++ src/typings/monaco.d.ts
@@ -1,7 +1,11 @@
 /// <reference path="../../node_modules/monaco-editor-core/monaco.d.ts"/>
 
-/** This is setup in our `index.html`. This just points to `require` function in monaco's loader (so it doesn't crash and burn our webpack workflow) */
-declare var monacoRequire: <T>(moduleId: [string], callback: (module: T) => void) => void;
+/**
+ * This is setup in our `index.html`. This just points to `require` function in monaco's loader
+ * Named differently so it doesn't crash and burn our webpack workflow.
+ * You can use it to load modules in sync because they are all already loaded internally when `editor.main` is loaded
+ */
+declare var monacoRequire: <T>(moduleId: [string]) => T;
 
 /** Makes bringing monaco code in easier */
 interface IDisposable {
