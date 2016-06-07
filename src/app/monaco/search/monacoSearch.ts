a8c518ca9d8aa358f26ff816acce86aef02dde68
Basarat <basaratali@gmail.com>
2016-06-07 18:28:57 +1000

    WIP find support

--- src/app/monaco/search/monacoSearch.ts
+++ src/app/monaco/search/monacoSearch.ts
@@ -5,23 +5,46 @@
 /** Some types */
 type Editor = monaco.editor.ICommonCodeEditor;
 
+declare global {
+    module monaco {
+        module editor {
+            interface ICommonCodeEditor {
+                _findController: CommonFindController;
+            }
+        }
+    }
+}
+
 /**
  * Mostly providing a typed API on top of `search`
  */
 export let commands = {
-    search: (cm: Editor, query: FindOptions) => startSearch(cm, getSearchState(cm), query),
+    search: (cm: Editor, query: FindOptions) => startSearch(cm, query),
     hideSearch: (cm: Editor) => hideSearch(cm),
-    findNext: (cm: Editor, query: FindOptions) => findNextIfNotAlreadyDoing(cm, query, false),
-    findPrevious: (cm: Editor, query: FindOptions) => findNextIfNotAlreadyDoing(cm, query, true),
+    findNext: (cm: Editor, query: FindOptions) => findNextIfNotAlreadyDoing(cm, query),
+    findPrevious: (cm: Editor, query: FindOptions) => findPreviousIfNotAlreadyDoing(cm, query),
     replaceNext: (cm: Editor, newText: string) => simpleReplace(cm, newText, false),
     replacePrevious: (cm: Editor, newText: string) => simpleReplacePrevious(cm, newText),
     replaceAll: (cm: Editor, newText: string) => simpleReplace(cm, newText, true),
 }
 
 /** TODO: mon */
-const getSearchState: any = () => null;
-const startSearch: any = () => null;
-const hideSearch: any = () => null;
-const findNextIfNotAlreadyDoing: any = () => null;
+const getSearchCtrl = (editor: Editor) => {
+    return CommonFindController.getFindController(editor);
+};
+const startSearch = (editor: Editor, query: FindOptions) => {
+    const ctrl = getSearchCtrl(editor);
+    ctrl.setSearchString(query.query);
+    // TODO: mon
+    // set other options as well
+};
+const hideSearch = (editor: Editor) => {
+    const ctrl = getSearchCtrl(editor);
+    ctrl.closeFindWidget();
+};
+const findNextIfNotAlreadyDoing = (editor: Editor, query: FindOptions) => null;
+const findPreviousIfNotAlreadyDoing = (editor: Editor, query: FindOptions) => null;
 const simpleReplace: any = () => null;
 const simpleReplacePrevious: any = () => null;
+
+import {CommonFindController} from "./findController";
