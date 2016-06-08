ce74028f77f57cd6c618f2fd8684653f6b907e40
Basarat Syed <basaratali@gmail.com>
2016-06-08 16:45:28 +1000

    WIP git status

---
+++ src/app/monaco/gitStatus.ts
@@ -0,0 +1,10 @@
+/** TIP */
+// static ADDED_DECORATION_OPTIONS: common.IModelDecorationOptions = {
+//     linesDecorationsClassName: 'git-dirty-added-diff-glyph',
+//     isWholeLine: true,
+//     overviewRuler: {
+//         color: 'rgba(0, 122, 204, 0.6)',
+//         darkColor: 'rgba(0, 122, 204, 0.6)',
+//         position: common.OverviewRulerLane.Left
+//     }
+// };
