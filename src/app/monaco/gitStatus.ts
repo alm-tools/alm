3c6dff5ae4a78c3a5788dabea2e103a82ea40141
Basarat <basaratali@gmail.com>
2016-06-08 19:41:40 +1000

    WIP git status

--- src/app/monaco/gitStatus.ts
+++ src/app/monaco/gitStatus.ts
@@ -10,8 +10,41 @@
 // };
 
 type Editor = monaco.editor.ICommonCodeEditor;
+import {CompositeDisposible} from "../../common/events";
+import * as utils from "../../common/utils";
+import {server} from "../../socket/socketClient";
 
 export function setup(cm: Editor): { dispose: () => void } {
     if (cm) return { dispose: () => null }; // DEBUG : while the feature isn't complete used to disable it
-    
+
+    const filePath = cm.filePath;
+
+    // this._editor.changeDecorations((changeAccessor: editorCommon.IModelDecorationsChangeAccessor) => {
+    //     if (this._highlightedDecorationId !== null) {
+    //         changeAccessor.changeDecorationOptions(this._highlightedDecorationId, FindDecorations.createFindMatchDecorationOptions(false));
+    //         this._highlightedDecorationId = null;
+    //     }
+    //     if (newCurrentDecorationId !== null) {
+    //         this._highlightedDecorationId = newCurrentDecorationId;
+    //         changeAccessor.changeDecorationOptions(this._highlightedDecorationId, FindDecorations.createFindMatchDecorationOptions(true));
+    //     }
+    // });
+
+    const refreshGitStatus = () => {
+        server.gitDiff({ filePath }).then((res) => {
+            // TODO: do the rest
+        });
+    }
+
+    const refreshGitStatusDebounced = utils.debounce(refreshGitStatus, 2000);
+
+    const handleFocus = () => {
+        refreshGitStatus();
+    }
+
+
+    const disposible = new CompositeDisposible();
+    disposible.add(cm.onDidFocusEditor(handleFocus));
+    disposible.add(cm.onDidChangeModel(refreshGitStatusDebounced));
+    return disposible;
 }
