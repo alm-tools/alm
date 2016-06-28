0bb94ef67eca3fa5b779a1d08d5a7466db0e6e28
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-06-28 19:30:59 +1000

    autocomplete wip

--- src/app/monaco/languages/json/autocomplete.ts
+++ src/app/monaco/languages/json/autocomplete.ts
@@ -1,3 +1,7 @@
+import * as utils from "../../../../common/utils";
+import * as monacoUtils from "../../monacoUtils";
+import {cast, server} from "../../../../socket/socketClient";
+
 import CancellationToken = monaco.CancellationToken;
 import Thenable = monaco.Thenable;
 import Position = monaco.Position;
@@ -8,16 +12,28 @@ export class CompletionAdapter implements monaco.languages.CompletionItemProvide
 		return [' ', ':'];
 	}
 
-	provideCompletionItems(model: monaco.editor.IReadOnlyModel, position: Position, token: CancellationToken): Thenable<monaco.languages.CompletionList> {
+    provideCompletionItems(model: monaco.editor.IReadOnlyModel, position: Position, token: CancellationToken): Thenable<monaco.languages.CompletionList> {
 		const wordInfo = model.getWordUntilPosition(position);
 		const filePath = model.filePath;
+        const prefix = wordInfo.word;
+        const offset = monacoUtils.positionToOffset(model, position);
 
         const result: monaco.languages.CompletionList = {
             isIncomplete: false,
             items:[]
         }
 
-		return Promise.resolve(result);
+        if (!utils.isSupportedConfigFileForAutocomplete(filePath)){
+            return Promise.resolve(result);
+        }
+
+        return server.getCompletionsAtPosition({filePath, position: offset, prefix}).then((res)=>{
+
+            // TODO: translate this to monaco completions
+            console.log(res);
+
+            return result;
+        });
 	}
 
 	resolveCompletionItem(item: monaco.languages.CompletionItem, token: CancellationToken): Thenable<monaco.languages.CompletionItem> {
