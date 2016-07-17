241a118ab81e71ef57539cbd341506bfa66aaf7a
Basarat Ali Syed <syedb@mpdmmac-70.gateway>
2016-07-17 13:18:14 +1000

    again :-/

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Move to production
-NODE_ENV="production"
-
 # Compile
 ./node_modules/.bin/ntsc -p ./src
 
 # Bundle
-./node_modules/.bin/webpack -p --config ./src/webpack.config.js
+./node_modules/.bin/webpack -p --define process.env.NODE_ENV='\"production\"' --config ./src/webpack.config.js
