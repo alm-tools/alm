b6c78bc6dd7f2f37b308dfb6217ae4054a47a9d6
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-07-26 08:32:13 +1000

    wip linter

--- src/server/workers/lint/lintWorker.ts
+++ src/server/workers/lint/lintWorker.ts
@@ -2,10 +2,12 @@ import * as sw from "../../utils/simpleWorker";
 import * as contract from "./lintContract";
 
 import {resolve} from "../../../common/utils";
+import * as Linter from "tslint";
 
 namespace Worker {
     export const activeProjectFilePaths: typeof contract.worker.activeProjectFilePaths = (data) => {
-        console.log('Got the file paths. Now I should start linting them.', data.filePaths.length);
+        // console.log('Linter got filePaths', data.filePaths.length); // DEBUG
+        LinterImplementation.setFilePaths(data.filePaths);
         return resolve({});
     }
 }
@@ -17,3 +19,28 @@ export const {master} = sw.runWorker({
     workerImplementation: Worker,
     masterContract: contract.master
 });
+
+
+namespace LinterImplementation {
+    let filePaths: string[] = [];
+
+    export function setFilePaths(_filePaths: string[]) {
+        filePaths = filePaths;
+        lintAgain();
+    }
+
+    /**
+     * We only create the program when we are ready to lint
+     */
+    let program: ts.Program | null = null;
+
+    function lintAgain() {
+        /**
+         * TODO: lint
+         * create the program
+         *
+         * create the Linter for each file and get its output
+         *
+         */
+    }
+}
