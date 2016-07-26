899cdc035c528a1785147db0186d5fa76612bda9
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-07-27 08:38:10 +1000

    master : better :memo:

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -477,7 +477,7 @@ export function timer() {
             const seconds = Math.ceil((new Date().getTime() - timeStart) / 1000) + 's';
             return seconds;
         },
-        /** Milliseconds */
+        /** Milliseconds e.g. 2000ms etc. */
         get ms() {
             const ms = (new Date().getTime() - timeStart) + 'ms';
             return ms;
