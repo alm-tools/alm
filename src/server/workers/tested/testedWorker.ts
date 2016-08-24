2febba4485856940414dc156f6c6550d9e3312a9
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-08-25 08:11:19 +1000

    tested.json parsing

--- src/server/workers/tested/testedWorker.ts
+++ src/server/workers/tested/testedWorker.ts
@@ -2,6 +2,8 @@ import * as sw from "../../utils/simpleWorker";
 import * as types from "../../../common/types";
 import * as contract from "./testedContract";
 import {resolve} from "../../../common/utils";
+import * as fsu from "../../utils/fsu";
+import {parse, parseErrorToCodeError} from "../../../common/json";
 
 namespace Worker {
     export const fileSaved: typeof contract.worker.fileSaved = (data) => {
@@ -17,3 +19,59 @@ export const {master} = sw.runWorker({
     workerImplementation: Worker,
     masterContract: contract.master
 });
+
+import {ErrorsCache} from "../../utils/errorsCache";
+const errorCache = new ErrorsCache();
+errorCache.errorsDelta.on(master.receiveErrorCacheDelta);
+
+namespace TestedWorkerImplementation {
+    type TestedJsonRaw = {
+        tests: {
+            include: string[],
+            exclude: string[],
+        }
+    }
+
+    type TestedJson = {
+        filePaths: string[];
+    }
+
+    const globalState = {
+        started: false,
+
+    }
+    function reinit() {
+        globalState.started = false;
+        errorCache.clearErrors();
+    }
+
+    /**
+     * Restart if:
+     * - tested.json changes
+     * - working directory changes
+     */
+    export function restart() {
+        reinit();
+        let testedJsonFilePath: string;
+        try {
+            testedJsonFilePath = fsu.travelUpTheDirectoryTreeTillYouFind(process.cwd(), 'tested.json');
+        }
+        catch (err) {
+            // Leave disabled
+            return;
+        }
+
+        // Validate tested.json
+        const parsed = parse<TestedJsonRaw>(testedJsonFilePath);
+        if (parsed.error){
+            errorCache.setErrorsByFilePaths(
+                [testedJsonFilePath],
+                [parseErrorToCodeError(testedJsonFilePath,parsed.error)]
+            );
+            return;
+        }
+
+        const rawData = parsed.data;
+        /** TODO: tested expand raw data into real data */
+    }
+}
