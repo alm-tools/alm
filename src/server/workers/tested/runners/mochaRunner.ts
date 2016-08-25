274e6c15a830a70dfa6b22c9e7e3aa9620227837
Basarat Ali Syed <syedb@mpdmmac-70.dpost.local>
2016-08-25 18:27:54 +1000

    starting a mocha runner

---
+++ src/server/workers/tested/runners/mochaRunner.ts
@@ -0,0 +1,16 @@
+/**
+ * @module
+ */
+import * as types from "../../../common/types";
+
+/**
+ * Takes a file name and runs it with ts-node + mocha and
+ * returns its parsed test output
+ */
+export function runTest(filePath: string): Promise<types.TestModule> {
+    const result: types.TestModule = {
+        filePath: filePath,
+        suites: []
+    }
+    return Promise.resolve(result);
+}
