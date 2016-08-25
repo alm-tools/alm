315b2a32b54c1177e9e7593504912ab31a35f1b6
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-08-26 08:58:29 +1000

    will sort it out later

--- src/server/workers/tested/runners/mochaRunner.ts
+++ src/server/workers/tested/runners/mochaRunner.ts
@@ -2,15 +2,53 @@
  * @module
  */
 import * as types from "../../../../common/types";
+import * as cp from "child_process";
+import * as utils from "../../../../common/utils";
+import * as fsu from "../../../utils/fsu";
+
+const tsNodeCompilerOptions = JSON.stringify({
+    allowJs: true,
+    target: 'es6',
+    module: 'commonjs'
+});
+
+/** Main utility function to execute a command */
+let mochaExec = (filePath:string): Promise<string> => {
+    /** Find key paths */
+    const nodeModulesFolder = fsu.travelUpTheDirectoryTreeTillYouFind(__dirname, "node_modules");
+    const tsNodePath = `${nodeModulesFolder}/.bin/ts-node`;
+    const mochaPath = `${nodeModulesFolder}/.bin/mocha`;
+
+    /** Execute this */
+    const toExec
+        = `node ${tsNodePath} --compilerOptions '${tsNodeCompilerOptions}' ${mochaPath} ${filePath}`;
+
+    console.log("Will Exec", toExec)
+
+    /** In this dir */
+    const cwd = utils.getDirectory(filePath);
+
+    return new Promise((resolve, reject) => {
+        cp.exec(toExec, { cwd: cwd }, (err, stdout, stderr) => {
+            if (stderr.toString().trim().length) {
+                return resolve(stderr.toString());
+            }
+            return resolve(stdout);
+        });
+    });
+}
+
 
 /**
  * Takes a file name and runs it with ts-node + mocha and
  * returns its parsed test output
  */
 export function runTest(filePath: string): Promise<types.TestModule> {
-    const toExec = "node_modules/bin/ts-node node_module/bin/mocha filePath";
 
     /** TODO: tested actually run the test */
+    mochaExec(filePath).then(res=>{
+        console.log(res);
+    })
 
     const result: types.TestModule = {
         filePath: filePath,
