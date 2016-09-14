835b2bdb95adb163bd3eb901c9fce9afd73ed52d
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-09-14 18:46:04 +1000

    setup for testing

---
+++ tests/success/removeUnusedImports/foo.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.useMe1 = 123;
+exports.useMe2 = 123;
+exports.dontUseMe1 = 123;
+exports.dontUseMe2 = 123;
---
+++ tests/success/removeUnusedImports/foo.ts
@@ -0,0 +1,13 @@
+/**
+ * Will be used
+ */
+export var useMe1 = 123;
+export var useMe2 = 123;
+export type UseMe1 = 123;
+
+/**
+ * Will not be used
+ */
+export var dontUseMe1 = 123;
+export var dontUseMe2 = 123;
+export type DontUseMe1 = 123;
