9ae9818480fe86e9a6ee1e993eaabd41f452651a
Basarat Ali Syed <syedb@mpdmmac-70.dpost.local>
2016-09-15 09:30:44 +1000

    my plan

--- tests/success/removeUnusedImports/iImport.ts
+++ tests/success/removeUnusedImports/iImport.ts
@@ -1,3 +1,7 @@
+/**
+ * The test plan is to run the command on this file.
+ * And see the git diff. It should only have `dont*` variables.
+ */
 import {
     dontUseMe1,
     dontUseMe2,
@@ -15,3 +19,10 @@ import * as useMeLocal1 from "./iExport";
 import * as dontUseMeLocal1 from "./iExport";
 import useMeLocal2 = require("./iExport");
 import dontUseMeLocal2 = require("./iExport");
+
+const ensureUsage =
+    useMe1
+    || useMeLocal1
+    || useMeLocal2;
+
+type EnsureUsage = UseMe1 | typeof useMe2;
