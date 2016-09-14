67bdb61c0c61a57628368688524f5471ec7b4b01
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-09-14 18:49:23 +1000

    plan

--- src/server/workers/lang/modules/removeUnusedImports.ts
+++ src/server/workers/lang/modules/removeUnusedImports.ts
@@ -1,6 +1,14 @@
 /**
- * Removes unused imports
+ * Removes unused imports (both import/require and ES6)
  */
-export const removeUnusedImports = (filePath: string, program: ts.Program) => {
+export const removeUnusedImports = (filePath: string, service: ts.LanguageService) => {
+
+    /**
+     * Plan:
+     * - First finds all the imports in the file
+     * - Then checks if they have any usages (using document highlighting).
+     * - For used ones it removes them
+     *   - If all the ones from a destructured ES6 import the whole import is removed
+     */
 
 }
