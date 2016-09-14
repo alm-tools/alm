792d879e3d5e9bc1ca941ab552ecea28a728e7f9
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-09-14 19:02:14 +1000

    better test

--- tests/success/removeUnusedImports/iImport.ts
+++ tests/success/removeUnusedImports/iImport.ts
@@ -6,6 +6,11 @@ import {
     useMe2,
     UseMe1
 } from './iExport';
+import {
+    dontUseMe1 as againDontUseMe1,
+    dontUseMe2 as againDontUseMe2,
+    DontUseMe1 as AgainDontUseMe1,
+} from './iExport';
 import * as useMeLocal1 from "./iExport";
 import * as dontUseMeLocal1 from "./iExport";
 import useMeLocal2 = require("./iExport");
