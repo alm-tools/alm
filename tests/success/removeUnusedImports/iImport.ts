babda021515c325fc510108d25aaaf0976c67e55
Basarat Ali Syed <syedb@mpdmmac-70.local>
2016-09-14 18:56:18 +1000

    better test

---
+++ tests/success/removeUnusedImports/iImport.ts
@@ -0,0 +1,12 @@
+import {
+    dontUseMe1,
+    dontUseMe2,
+    DontUseMe1,
+    useMe1,
+    useMe2,
+    UseMe1
+} from './iExport';
+import * as useMeLocal1 from "./iExport";
+import * as dontUseMeLocal1 from "./iExport";
+import useMeLocal2 = require("./iExport");
+import dontUseMeLocal2 = require("./iExport");
