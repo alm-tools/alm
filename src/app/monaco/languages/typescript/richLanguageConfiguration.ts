beb81700d72f3d7b3fb1aa9dfe41779322cd9a5f
Basarat Ali Syed <syedb@mpdmmac-70.local>
2017-01-02 13:15:54 +1100

    format file

--- src/app/monaco/languages/typescript/richLanguageConfiguration.ts
+++ src/app/monaco/languages/typescript/richLanguageConfiguration.ts
@@ -1,56 +1,56 @@
-export const richLanguageConfiguration:monaco.languages.LanguageConfiguration = {
-	wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
+export const richLanguageConfiguration: monaco.languages.LanguageConfiguration = {
+    wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
 
-	comments: {
-		lineComment: '//',
-		blockComment: ['/*', '*/']
-	},
+    comments: {
+        lineComment: '//',
+        blockComment: ['/*', '*/']
+    },
 
-	brackets: [
-		['{', '}'],
-		['[', ']'],
-		['(', ')']
-	],
+    brackets: [
+        ['{', '}'],
+        ['[', ']'],
+        ['(', ')']
+    ],
 
-	onEnterRules: [
-		{
-			// e.g. /** | */
-			beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-			afterText: /^\s*\*\/$/,
-			action: { indentAction: monaco.languages.IndentAction.IndentOutdent, appendText: ' * ' }
-		},
-		{
-			// e.g. /** ...|
-			beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-			action: { indentAction: monaco.languages.IndentAction.None, appendText: ' * ' }
-		},
-		{
-			// e.g.  * ...|
-			beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
-			action: { indentAction: monaco.languages.IndentAction.Indent, appendText: '* ' }
-		},
-		{
-			// e.g.  */|
-			beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
-			action: { indentAction: monaco.languages.IndentAction.None, removeText: 1 }
-		},
-		{
-			// e.g. //|
-			beforeText: /^\s*\/\/.*$/,
-			action: { indentAction: monaco.languages.IndentAction.Indent, appendText: '// ' }
-		},
-	],
+    onEnterRules: [
+        {
+            // e.g. /** | */
+            beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+            afterText: /^\s*\*\/$/,
+            action: { indentAction: monaco.languages.IndentAction.IndentOutdent, appendText: ' * ' }
+        },
+        {
+            // e.g. /** ...|
+            beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+            action: { indentAction: monaco.languages.IndentAction.None, appendText: ' * ' }
+        },
+        {
+            // e.g.  * ...|
+            beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
+            action: { indentAction: monaco.languages.IndentAction.Indent, appendText: '* ' }
+        },
+        {
+            // e.g.  */|
+            beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
+            action: { indentAction: monaco.languages.IndentAction.None, removeText: 1 }
+        },
+        {
+            // e.g. //|
+            beforeText: /^\s*\/\/.*$/,
+            action: { indentAction: monaco.languages.IndentAction.Indent, appendText: '// ' }
+        },
+    ],
 
-	__electricCharacterSupport: {
-		docComment: { open:'/**', close:' */'}
-	},
+    __electricCharacterSupport: {
+        docComment: { open: '/**', close: ' */' }
+    },
 
-	autoClosingPairs: [
-		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
-		{ open: '"', close: '"', notIn: ['string'] },
-		{ open: '\'', close: '\'', notIn: ['string', 'comment'] },
-		{ open: '`', close: '`' }
-	]
+    autoClosingPairs: [
+        { open: '{', close: '}' },
+        { open: '[', close: ']' },
+        { open: '(', close: ')' },
+        { open: '"', close: '"', notIn: ['string'] },
+        { open: '\'', close: '\'', notIn: ['string', 'comment'] },
+        { open: '`', close: '`' }
+    ]
 };
