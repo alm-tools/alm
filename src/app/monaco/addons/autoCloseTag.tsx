a3c30869a5782549322f3fb1fbf6a7c1ac83c776
Basarat Ali Syed <syedb@mpdmmac-70.dpost.local>
2017-01-11 12:11:38 +1100

    also auto close `<div(cursor)` user types `>` => `<div>(cursor)</div>

--- src/app/monaco/addons/autoCloseTag.tsx
+++ src/app/monaco/addons/autoCloseTag.tsx
@@ -22,85 +22,114 @@ export function setup(cm: Editor): { dispose: () => void } {
 }
 
 function insertAutoCloseTag(event: TextDocumentContentChangeEvent, editor: Editor): void {
-    /** We insert on `</` */
-    if (event.text !== "/") {
-        return;
-    }
     let originalRange = event.range;
-    let text = editor.getModel().getValueInRange({
-        startLineNumber: 1,
-        startColumn: 1,
-        endLineNumber: originalRange.endLineNumber,
-        endColumn: originalRange.endColumn,
-    });
 
-    let lastChar = "";
-    if (text.length > 2) {
-        lastChar = text.substr(text.length - 1);
-    }
-    if (lastChar !== "<") {
-        return;
-    }
+    /** User just did `<foo>` */
+    if (event.text === ">") {
+        let closeTag = getCloseTagIfAtAnOpenOne(editor.filePath, editor.getModel().getOffsetAt({
+            lineNumber: originalRange.endLineNumber,
+            column: originalRange.endColumn
+        }));
 
-    /** Yay, we have </ See if we have a close tag ? */
-    let closeTag = getCloseTag(editor.filePath, editor.getModel().getOffsetAt({
-        lineNumber: originalRange.endLineNumber,
-        column: originalRange.endColumn
-    }));
+        if (!closeTag) return;
 
-    if (!closeTag) {
-        return;
-    }
-    /** Yay we have candidate closeTag like `div` */
+        closeTag = `</${closeTag}>`;
 
-    /**
-     * If the user already has a trailing `>` e.g.
-     * before: <div><(pos)>
-     * after: <div><(pos)/>
-     * Next chars will be `/>`
-     */
-    const nextChars = getNext2Chars(editor, { lineNumber: originalRange.endLineNumber, column: originalRange.endColumn });
-
-    /** If the next chars are not `/>` then we want to complete `>` for the user as well */
-    if (nextChars !== "/>") {
-        closeTag = closeTag + '>';
-    }
-
-    /** Make edits */
-    const startAt = editor.getModel().modifyPosition({
-        lineNumber: originalRange.endLineNumber,
-        column: originalRange.endColumn
-    }, 1);
-    monacoUtils.replaceRange({
-        model: editor.getModel(),
-        range: {
-            startLineNumber: startAt.lineNumber,
-            startColumn: startAt.column,
-            endLineNumber: startAt.lineNumber,
-            endColumn: startAt.column
-        },
-        newText: closeTag
-    });
-
-    /** And advance the cursor */
-    let endAt = editor.getModel().modifyPosition({
-        lineNumber: startAt.lineNumber,
-        column: startAt.column
-    }, closeTag.length);
-    if (nextChars === "/>") {
-        /** Advance one char more */
-        endAt = editor.getModel().modifyPosition(endAt, 1)
-    }
-    /** Set timeout. Because it doesn't work otherwise */
-    setTimeout(() => {
-        editor.setSelection({
-            startLineNumber: endAt.lineNumber,
-            startColumn: endAt.column,
-            endLineNumber: endAt.lineNumber,
-            endColumn: endAt.column,
+        /** Make edits */
+        const startAt = editor.getModel().modifyPosition({
+            lineNumber: originalRange.endLineNumber,
+            column: originalRange.endColumn
+        }, 1);
+        monacoUtils.replaceRange({
+            model: editor.getModel(),
+            range: {
+                startLineNumber: startAt.lineNumber,
+                startColumn: startAt.column,
+                endLineNumber: startAt.lineNumber,
+                endColumn: startAt.column
+            },
+            newText: closeTag
         });
-    });
 
+        return;
+    }
+
+    /** User just did `<foo>something</` */
+    if (event.text === "/") {
+        let text = editor.getModel().getValueInRange({
+            startLineNumber: 1,
+            startColumn: 1,
+            endLineNumber: originalRange.endLineNumber,
+            endColumn: originalRange.endColumn,
+        });
+
+        let lastChar = "";
+        if (text.length > 2) {
+            lastChar = text.substr(text.length - 1);
+        }
+        if (lastChar !== "<") {
+            return;
+        }
+
+        /** Yay, we have </ See if we have a close tag ? */
+        let closeTag = getCloseTagForAnAlreadyOpenOne(editor.filePath, editor.getModel().getOffsetAt({
+            lineNumber: originalRange.endLineNumber,
+            column: originalRange.endColumn
+        }));
+
+        if (!closeTag) {
+            return;
+        }
+        /** Yay we have candidate closeTag like `div` */
+
+        /**
+         * If the user already has a trailing `>` e.g.
+         * before: <div><(pos)>
+         * after: <div><(pos)/>
+         * Next chars will be `/>`
+         */
+        const nextChars = getNext2Chars(editor, { lineNumber: originalRange.endLineNumber, column: originalRange.endColumn });
+
+        /** If the next chars are not `/>` then we want to complete `>` for the user as well */
+        if (nextChars !== "/>") {
+            closeTag = closeTag + '>';
+        }
+
+        /** Make edits */
+        const startAt = editor.getModel().modifyPosition({
+            lineNumber: originalRange.endLineNumber,
+            column: originalRange.endColumn
+        }, 1);
+        monacoUtils.replaceRange({
+            model: editor.getModel(),
+            range: {
+                startLineNumber: startAt.lineNumber,
+                startColumn: startAt.column,
+                endLineNumber: startAt.lineNumber,
+                endColumn: startAt.column
+            },
+            newText: closeTag
+        });
+
+        /** And advance the cursor */
+        let endAt = editor.getModel().modifyPosition({
+            lineNumber: startAt.lineNumber,
+            column: startAt.column
+        }, closeTag.length);
+        if (nextChars === "/>") {
+            /** Advance one char more */
+            endAt = editor.getModel().modifyPosition(endAt, 1)
+        }
+        /** Set timeout. Because it doesn't work otherwise */
+        setTimeout(() => {
+            editor.setSelection({
+                startLineNumber: endAt.lineNumber,
+                startColumn: endAt.column,
+                endLineNumber: endAt.lineNumber,
+                endColumn: endAt.column,
+            });
+        });
+    }
 }
 
 function getNext2Chars(editor: Editor, position: monaco.IPosition): string {
@@ -115,7 +144,7 @@ function getNext2Chars(editor: Editor, position: monaco.IPosition): string {
 }
 
 import { getSourceFile } from '../model/classifierCache';
-function getCloseTag(filePath: string, position: number): string | null {
+function getCloseTagForAnAlreadyOpenOne(filePath: string, position: number): string | null {
     const sourceFile = getSourceFile(filePath);
     const opens: ts.JsxOpeningElement[] = [];
 
@@ -148,6 +177,53 @@ function getCloseTag(filePath: string, position: number): string | null {
         const tabToClose = opens[opens.length - 1]; // close the last one first
         const tagName = tabToClose.tagName.getText(); // something like `foo.Someting`
         return tagName;
+    }
+
+    return null;
+}
+function getCloseTagIfAtAnOpenOne(filePath: string, position: number): string | null {
+    const sourceFile = getSourceFile(filePath);
+    let found: ts.JsxSelfClosingElement | null = null;
+
+    const collectTags = (node: ts.Node) => {
+        /**
+         * <div
+         * Is actually parsed as a JSX self closing tag
+         **/
+        if (node.kind === ts.SyntaxKind.JsxSelfClosingElement) {
+            /**
+             * With
+             * <foo>
+             *   <another(cursor)
+             * </foo>
+             *
+             * the `<another </` is what the jsx self closing tag contains
+             * So if its a *self closing* tag with a start before and end after ... its a candidate
+             */
+            if (!(node.getStart() <= position) && !(node.getEnd() >= position)) return;
+
+            const fullText = node.getFullText().trim();
+
+            // is actually closed
+            if (fullText.endsWith('/')
+                /**
+                 * Is getting the next `</` e.g.
+                 * <foo>
+                 *   <another(cursor)
+                 * </foo>
+                 * is parsed as
+                 * `<another </`
+                 */
+                && !fullText.endsWith('</')) return;
+
+            found = node as ts.JsxSelfClosingElement;
+        }
+        ts.forEachChild(node, collectTags);
+    }
+    ts.forEachChild(sourceFile, collectTags);
+
+    if (found) {
+        return found.tagName.getText();
     }
 
     return null;
