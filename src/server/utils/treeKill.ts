a3e54416d4ced4eef2e354176b441ea85fdad95c
Basarat Ali Syed <syedb@mpdmmac-70.local>
2017-01-22 12:32:18 +1100

    make treeKill promise based

--- src/server/utils/treeKill.ts
+++ src/server/utils/treeKill.ts
@@ -6,96 +6,101 @@ import childProcess = require('child_process');
 const spawn = childProcess.spawn;
 const exec = childProcess.exec;
 
-export function kill(pid: number, signal: 'SIGTERM' | 'SIGINT' = 'SIGTERM', callback: () => void = () => null) {
-  var tree = {};
-  var pidsToProcess = {};
-  tree[pid] = [];
-  pidsToProcess[pid] = 1;
+export function kill(pid: number, signal: 'SIGTERM' | 'SIGINT' = 'SIGTERM'): Promise<void> {
+    let resolve: () => void;
+    const res = new Promise<void>((res) => resolve = res);
 
-  switch (process.platform) {
-    case 'win32':
-      exec('taskkill /pid ' + pid + ' /T /F', callback);
-      break;
-    case 'darwin':
-      buildProcessTree(pid, tree, pidsToProcess, function(parentPid) {
-        return spawn('pgrep', ['-P', parentPid]);
-      }, function() {
-        killAll(tree, signal, callback);
-      });
-      break;
-    default: // Linux
-      buildProcessTree(pid, tree, pidsToProcess, function(parentPid) {
-        return spawn('ps', ['-o', 'pid', '--no-headers', '--ppid', parentPid]);
-      }, function() {
-        killAll(tree, signal, callback);
-      });
-      break;
-  }
+    var tree = {};
+    var pidsToProcess = {};
+    tree[pid] = [];
+    pidsToProcess[pid] = 1;
+
+    switch (process.platform) {
+        case 'win32':
+            exec('taskkill /pid ' + pid + ' /T /F', resolve);
+            break;
+        case 'darwin':
+            buildProcessTree(pid, tree, pidsToProcess, function(parentPid) {
+                return spawn('pgrep', ['-P', parentPid]);
+            }, function() {
+                killAll(tree, signal, resolve);
+            });
+            break;
+        default: // Linux
+            buildProcessTree(pid, tree, pidsToProcess, function(parentPid) {
+                return spawn('ps', ['-o', 'pid', '--no-headers', '--ppid', parentPid]);
+            }, function() {
+                killAll(tree, signal, resolve);
+            });
+            break;
+    }
+
+    return res;
 };
 
 function killAll(tree, signal, callback) {
-  var killed = {};
-  try {
-    Object.keys(tree).forEach(function(pid) {
-      tree[pid].forEach(function(pidpid) {
-        if (!killed[pidpid]) {
-          killPid(pidpid, signal);
-          killed[pidpid] = 1;
+    var killed = {};
+    try {
+        Object.keys(tree).forEach(function(pid) {
+            tree[pid].forEach(function(pidpid) {
+                if (!killed[pidpid]) {
+                    killPid(pidpid, signal);
+                    killed[pidpid] = 1;
+                }
+            });
+            if (!killed[pid]) {
+                killPid(pid, signal);
+                killed[pid] = 1;
+            }
+        });
+    } catch (err) {
+        if (callback) {
+            return callback(err);
+        } else {
+            throw err;
         }
-      });
-      if (!killed[pid]) {
-        killPid(pid, signal);
-        killed[pid] = 1;
-      }
-    });
-  } catch (err) {
-    if (callback) {
-      return callback(err);
-    } else {
-      throw err;
     }
-  }
-  if (callback) {
-    return callback();
-  }
+    if (callback) {
+        return callback();
+    }
 }
 
 function killPid(pid, signal) {
-  try {
-    process.kill(parseInt(pid, 10), signal);
-  }
-  catch (err) {
-    if (err.code !== 'ESRCH') throw err;
-  }
+    try {
+        process.kill(parseInt(pid, 10), signal);
+    }
+    catch (err) {
+        if (err.code !== 'ESRCH') throw err;
+    }
 }
 
 function buildProcessTree(parentPid, tree, pidsToProcess, spawnChildProcessesList, cb) {
-  var ps = spawnChildProcessesList(parentPid);
-  var allData = '';
-  ps.stdout.on('data', function(data) {
-    var data = data.toString('ascii');
-    allData += data;
-  });
-
-  var onClose = function(code) {
-    delete pidsToProcess[parentPid];
-
-    if (code != 0) {
-      // no more parent processes
-      if (Object.keys(pidsToProcess).length == 0) {
-        cb();
-      }
-      return;
-    }
-
-    allData.match(/\d+/g).forEach(function(_pid) {
-      let pid = parseInt(_pid, 10);
-      tree[parentPid].push(pid);
-      tree[pid] = [];
-      pidsToProcess[pid] = 1;
-      buildProcessTree(pid, tree, pidsToProcess, spawnChildProcessesList, cb);
+    var ps = spawnChildProcessesList(parentPid);
+    var allData = '';
+    ps.stdout.on('data', function(data) {
+        var data = data.toString('ascii');
+        allData += data;
     });
-  };
 
-  ps.on('close', onClose);
+    var onClose = function(code) {
+        delete pidsToProcess[parentPid];
+
+        if (code != 0) {
+            // no more parent processes
+            if (Object.keys(pidsToProcess).length == 0) {
+                cb();
+            }
+            return;
+        }
+
+        allData.match(/\d+/g).forEach(function(_pid) {
+            let pid = parseInt(_pid, 10);
+            tree[parentPid].push(pid);
+            tree[pid] = [];
+            pidsToProcess[pid] = 1;
+            buildProcessTree(pid, tree, pidsToProcess, spawnChildProcessesList, cb);
+        });
+    };
+
+    ps.on('close', onClose);
 }
---
+++ src/server/workers/external/demoReact/demoReactService.ts
@@ -0,0 +1,33 @@
+import { TypedEvent } from '../../../../common/events';
+import { kill } from '../../../utils/treeKill';
+import { GetPort } from '../../../utils/getPort';
+
+const workerPrefix = `[DEMO-REACT]`;
+
+export namespace WorkerImplementation {
+    export let currentFilePath = '';
+    export const clearLiveDemo = new TypedEvent<{}>();
+    export const liveDemoData = new TypedEvent<{ data: string }>();
+
+    export const enableLiveDemo = ({filePath}: { filePath: string }) => {
+        console.log(workerPrefix, `Started on filePath: ${filePath}`);
+        // if (executor) {
+        //     executor.dispose();
+        // }
+        clearLiveDemo.emit({});
+        // executor = new FileExecutor(filePath, (data) => {
+        //     liveDemoData.emit({ data });
+        // });
+        currentFilePath = filePath;
+        return Promise.resolve({})
+    };
+    export const disableLiveDemo = () => {
+        // if (executor) {
+        //     clearLiveDemo.emit({});
+        //     executor.dispose();
+        //     executor = undefined;
+        //     currentFilePath = '';
+        // }
+        return Promise.resolve({})
+    }
+}
