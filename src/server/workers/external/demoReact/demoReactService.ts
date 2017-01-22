d9ebde1d4f301580f5ca9681d74be4e41656335e
Basarat Ali Syed <syedb@mpdmmac-70.local>
2017-01-22 14:04:07 +1100

    live demo index

--- src/server/workers/external/demoReact/demoReactService.ts
+++ src/server/workers/external/demoReact/demoReactService.ts
@@ -5,8 +5,30 @@ import { appSettingsFolder } from '../../../disk/settings';
 import * as mkdirp from 'mkdirp';
 
 const workerPrefix = `[DEMO-REACT]`;
+
+/**
+ * This is where we write our index.html plus app.js
+ */
 const liveDemoFolder = appSettingsFolder + '/liveDemoReact';
 mkdirp.sync(liveDemoFolder);
+
+const appIndexTemplate = ({ jsFileName }: { jsFileName: string}) =>
+    `
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+    <title>ALM Live React Demo</title>
+</head>
+<body>
+  <div id="root"></div>
+  <script type="text/javascript" src="./${jsFileName}"></script>
+</body>
+</html>
+`;
 
 export namespace WorkerImplementation {
     export let currentFilePath = '';
